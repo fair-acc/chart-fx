b71afcd4fd6955dd78be12f914e466811cc4a75f
Alexander Krimm <A.Krimm@gsi.de>
2019-06-28 13:28:13 +0200

    Add documentation and default behavior to JavaFX Launch Helper

--- chartfx-samples/src/main/java/de/gsi/samples/util/LaunchJFX.java
+++ chartfx-samples/src/main/java/de/gsi/samples/util/LaunchJFX.java
@@ -3,29 +3,26 @@ package de.gsi.samples.util;
 import de.gsi.chart.samples.RunChartSamples;
 import javafx.application.Application;
 
-// for library loggers
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-
-// for application loggers
-//import de.gsi.cs.co.ap.common.gui.elements.logger.AppLogger;
-
 /**
- *
+ * Helper class to launch JavaFX applications without manually adding modules and exports
+ * to the java command line.
+ * Add a run configuration for this class in eclipse and in the arguments tab, add "${java_type_name}" as program
+ * argument. Then, any JavaFX Application Class can be run by selecting it in the Package Explorer and running this
+ * run configuration. <br>
+ * To be able to run JavaFX Applications outside of the chartfx project also add a classpath entry of type "Advanced",
+ * "Variable", "${project_classpat}", but there is no way to add the current projects dependencies to the classpath,
+ * yet.
+ * 
+ * @see <a href="https://stackoverflow.com/a/55300492" target="_top">
+ *      Stackoverflow: How to add JavaFX runtime to Eclipse in Java11 (2b) </a>
  * @author akrimm
  */
 public class LaunchJFX {
-
-    // You can choose a logger (needed imports are given in the import section as comments):
-    // for libraries:
-    // private static final Logger LOGGER = LoggerFactory.getLogger(TestFX11.class);
-    // for swing applications:
-    // private static final AppLogger LOGGER = AppLogger.getLogger();
-    // for fx applications:
-    // private static final AppLogger LOGGER = AppLogger.getFxLogger();
     public static void main(final String[] args) throws ClassNotFoundException {
-        //Application.launch(RunChartSamples.class);
-        Application.launch(Class.forName(args[0]).asSubclass(Application.class));
+        if (args.length < 1) {
+            Application.launch(RunChartSamples.class);
+        } else {
+            Application.launch(Class.forName(args[0]).asSubclass(Application.class));
+        }
     }
 }
-
