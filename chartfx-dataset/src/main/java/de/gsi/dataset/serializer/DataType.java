671514509f83518e2d284865a99c298b0eb01055
rstein <r.steinhagen@gsi.de>
2019-09-03 13:48:09 +0200

    added java-specific data type name + reformatting

--- chartfx-dataset/src/main/java/de/gsi/dataset/serializer/DataType.java
+++ chartfx-dataset/src/main/java/de/gsi/dataset/serializer/DataType.java
@@ -11,43 +11,46 @@ import java.util.Map;
 public enum DataType {
     // @formatter:off
     // primitive types
-    BOOL                     (0     ,"bool"                    , 1, Boolean.class, true),
-    BYTE                     (1     ,"byte"                    , 1, Byte.class, true),
-    SHORT                    (2     ,"short"                   , 2, Short.class, true),
-    INT                      (3     ,"int"                     , 4, Integer.class, true),
-    LONG                     (4     ,"long"                    , 8, Long.class, true),
-    FLOAT                    (5     ,"float"                   , 4, Float.class, true),
-    DOUBLE                   (6     ,"double"                  , 8, Double.class, true),
-    CHAR                     (7     ,"char"                    , 2, Character.class, true),
-    STRING                   (8     ,"string"                  , 1, String.class, true),
+    BOOL                     (0     ,"bool"           ,"boolean",           1, Boolean.class, true),
+    BYTE                     (1     ,"byte"           ,"byte",              1, Byte.class, true),
+    SHORT                    (2     ,"short"          ,"short",             2, Short.class, true),
+    INT                      (3     ,"int"            ,"int",               4, Integer.class, true),
+    LONG                     (4     ,"long"           ,"long",              8, Long.class, true),
+    FLOAT                    (5     ,"float"          ,"float",             4, Float.class, true),
+    DOUBLE                   (6     ,"double"         ,"double",            8, Double.class, true),
+    CHAR                     (7     ,"char"           ,"char",              2, Character.class, true),
+    STRING                   (8     ,"string"         ,"string",            1, String.class, true),
 
     // array of primitive types
-    BOOL_ARRAY               (100   ,"bool_array"              , 1, Boolean[].class, false),
-    BYTE_ARRAY               (101   ,"byte_array"              , 1, Byte[].class, false),
-    SHORT_ARRAY              (102   ,"short_array"             , 2, Short[].class, false),
-    INT_ARRAY                (103   ,"int_array"               , 4, Integer[].class, false),
-    LONG_ARRAY               (104   ,"long_array"              , 8, Long[].class, false),
-    FLOAT_ARRAY              (105   ,"float_array"             , 4, Float[].class, false),
-    DOUBLE_ARRAY             (106   ,"double_array"            , 8, Double[].class, false),
-    CHAR_ARRAY               (107   ,"char"                    , 2, Character[].class, true),
-    STRING_ARRAY             (108   ,"string_array"            , 1, String[].class, false),
+    BOOL_ARRAY               (100   ,"bool_array"     ,"[Z",                1, Boolean[].class, false),
+    BYTE_ARRAY               (101   ,"byte_array"     ,"[B",                1, Byte[].class, false),
+    SHORT_ARRAY              (102   ,"short_array"    ,"[S",                2, Short[].class, false),
+    INT_ARRAY                (103   ,"int_array"      ,"[I",                4, Integer[].class, false),
+    LONG_ARRAY               (104   ,"long_array"     ,"[J",                8, Long[].class, false),
+    FLOAT_ARRAY              (105   ,"float_array"    ,"[F",                4, Float[].class, false),
+    DOUBLE_ARRAY             (106   ,"double_array"   ,"[D",                8, Double[].class, false),
+    CHAR_ARRAY               (107   ,"char_array"     ,"[C",                2, Character[].class, true),
+    STRING_ARRAY             (108   ,"string_array"   ,"[java.lang.String", 1, String[].class, false),
 
     // complex objects
-    MAP                      (200   ,"map"                     , 1, Map.class, false),
+    MAP                      (200   ,"map"            ,"", 1, Map.class, false),
 
-    END_MARKER               (0xFE  ,"end_marker"              , 1, Byte.class, false);
+    END_MARKER               (0xFE  ,"end_marker"     ,"", 1, Byte.class, false);
     // @formatter:on
-
+	
     private final byte byteValue;
     private final long primitiveSize;
     private final String stringValue;
+    private final String javaName;
     private final Class<?> classType;
     private final boolean scalar;
+    
 
-    DataType(final int byteValue, final String stringValue, final long primitiveSize, Class<?> classType,
+    DataType(final int byteValue, final String stringValue, final String javaName, final long primitiveSize, Class<?> classType,
             boolean isScalar) {
         this.byteValue = (byte) byteValue;
         this.stringValue = stringValue;
+        this.javaName = javaName;
         this.primitiveSize = primitiveSize;
         this.classType = classType;
         this.scalar = isScalar;
@@ -98,6 +101,21 @@ public enum DataType {
         }
         throw new IllegalArgumentException("Entry type '" + str + "' is not supported");
     }
+    
+    /**
+     * Returns the data type matching the given java string representation, if any.
+     * 
+     * @param str the string to be searched
+     * @return the matching data type
+     */
+    public static DataType fromJavaTypeString(String str) {
+        for (DataType type : DataType.values()) {
+            if (type.stringValue.equals(str)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Entry type '" + str + "' is not supported");
+    }
 
     /**
      * Returns the byte representation of the data type.
@@ -116,6 +134,17 @@ public enum DataType {
     public String getAsString() {
         return this.stringValue;
     }
+    
+    
+    /**
+     * Returns the string representation of the java class type.
+     * 
+     * @return the string representation of the class
+     */
+    public String getJavaName() {
+        return this.javaName;
+    }
+    
 
     /**
      * Returns the corresponding java class type matching the given data type
