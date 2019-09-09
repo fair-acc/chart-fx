1a37283bad2088e43b2ed5b322437e63911a1904
rstein <r.steinhagen@gsi.de>
2019-09-09 18:14:37 +0200

    added DataSetLock interface and implementation prototypes and tests

---
+++ chartfx-dataset/src/main/java/de/gsi/dataset/locks/DataSetLock.java
@@ -0,0 +1,118 @@
+package de.gsi.dataset.locks;
+
+import java.io.Serializable;
+import java.util.function.Supplier;
+
+import de.gsi.dataset.DataSet;
+
+/**
+ * A Simple ReadWriteLock for the DataSet interface and its fluent-design
+ * approach
+ * 
+ * Some implementation recommendation: write lock guards behave the same as
+ * ReentrantLock with the additional functionality, that a
+ * <code>writeLock()</code> and subsequent <code>writeUnLock()</code> mute and,
+ * respectively, un-mute the given DataSet's auto-notification states, e.g.
+ * example:
+ * 
+ * <pre>
+ *  lock.writeLock(); // stores isAutoNotification state
+ *     [..] some other code [..]
+ *  lock.writeUnLock(); // restores isAutoNotification state
+ * </pre>
+ * 
+ * However, the recommended usage is using the lock guard primitives, e.g.
+ * 
+ * <pre>
+ * lock.readLockGuard(() -&gt; {
+ *    [..] some read-lock protected code [..]
+ *    return retVal; // N.B. optional return - here: assumes Objects or boxed primitives
+ * });
+ * </pre>
+ *
+ * Alternatively the best performing option for frequent simple reads without
+ * major data processing
+ * 
+ * <pre>
+ * Result ret = lock.readLockGuardOptimistic(() -&gt; {
+ *    [..] some read-lock protected code [..]
+ * 	  return retVal; // N.B. optional return - here: assumes Objects or boxed primitives
+ * });
+ * </pre>
+ * 
+ * The latter assumes infrequent writes (e.g. a single writer thread) and
+ * frequent unobstructed reads (ie. many reader threads). The lock internally
+ * acquires the data w/o explicitly locking, checks afterwards if the data has
+ * potentially changed a write-lock acquiring thread, and as a automatic
+ * fall-back uses the guaranteed (but more expensive) read lock to assure that
+ * the read data structure is consistent.
+ * 
+ * @author rstein
+ * @param <D> generics reference, usually to
+ *            <code>&lt;? extends DataSet&gt;</code>
+ */
+@SuppressWarnings({ "PMD.DoNotUseThreads", "PMD.CommentSize" }) // Runnable used as functional interface
+public interface DataSetLock<D extends DataSet> extends Serializable {
+
+	/**
+	 * reentrant read-lock
+	 * 
+	 * @return supporting DataSet (fluent design)
+	 */
+	D readLock();
+
+	/**
+	 * @param reading typ. lambda expression that is executed with read lock
+	 * @return supporting DataSet (fluent design)
+	 */
+	D readLockGuard(final Runnable reading); //
+
+	/**
+	 * @param <R>     generic return type
+	 * @param reading typ. lambda expression that is executed with read lock
+	 * @return supporting DataSet (fluent design)
+	 */
+	<R> R readLockGuard(final Supplier<R> reading);
+
+	/**
+	 * @param reading typ. lambda expression that is executed with read lock
+	 * @return supporting DataSet (fluent design)
+	 */
+	D readLockGuardOptimistic(final Runnable reading);
+
+	/**
+	 * @param <R>     generic return type
+	 * @param reading typ. lambda expression that is executed with read lock
+	 * @return supporting DataSet (fluent design)
+	 */
+	<R> R readLockGuardOptimistic(final Supplier<R> reading);
+
+	/**
+	 * @return supporting DataSet (fluent design)
+	 */
+	D readUnLock();
+
+	/**
+	 * @return supporting DataSet (fluent design)
+	 */
+	D writeLock();
+
+	/**
+	 * @param writing typ. lambda expression that is executed with write lock
+	 * @return supporting DataSet (fluent design)
+	 */
+	D writeLockGuard(final Runnable writing);
+
+	/**
+	 * @param <R>     generic return type
+	 * @param writing typ. lambda expression that is executed with write lock
+	 * @return supporting DataSet (fluent design)
+	 */
+	<R> R writeLockGuard(final Supplier<R> writing);
+
+	/**
+	 * @return supporting DataSet (fluent design)
+	 */
+	D writeUnLock();
+
+}
---
+++ chartfx-dataset/src/test/java/de/gsi/dataset/locks/DataSetLockTest.java
@@ -0,0 +1,157 @@
+package de.gsi.dataset.locks;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.gsi.dataset.spi.DefaultDataSet;
+
+/**
+ * Tests of DataSetLock
+ * 
+ * @author rstein
+ * 
+ * @see de.gsi.dataset.locks.DataSetLock
+ * @see de.gsi.dataset.locks.DefaultDataSetLock
+ */
+public class DataSetLockTest {
+	private static final Logger LOGGER = LoggerFactory.getLogger(DataSetLockTest.class);
+
+	/**
+	 * coarse tests
+	 * N.B. to be refined
+	 */
+	@Test
+	@DisplayName("Tests DataSet lock default implementation")
+	public void testDataSetLock() {
+		DefaultDataSet dataSet = new DefaultDataSet("test");
+		DefaultDataSetLock<DefaultDataSet> myLockImpl = new DefaultDataSetLock<>(dataSet);
+		DataSetLock<DefaultDataSet> myLock = myLockImpl;
+
+		// fluent-api style:
+		// [..].writeLock().set(0, 0.0, 0.0).set(1, 1.0, 1.0).writeUnLock();
+
+		assertEquals(0, myLockImpl.getReaderCount());
+		assertEquals(0, myLockImpl.getWriterCount());
+		assertTrue(dataSet.isAutoNotification());
+		
+		myLock.writeLock().getDataCount();
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.atDebug().log("WRITER: after first lock");
+		}
+		assertEquals(0, myLockImpl.getReaderCount());
+		assertEquals(1, myLockImpl.getWriterCount());
+		
+		final int nListener = 3;
+		Thread[] threads = new Thread[nListener];
+		for (int i = 0; i < 3; i++) {
+			final int listener = i;
+			threads[i] = new Thread(() -> {
+				if (LOGGER.isDebugEnabled()) {
+					LOGGER.atDebug().addArgument(listener).log("READER: try to acquire lock for listener #{}");
+				}
+				myLock.readLock();
+				sleep(200);
+				if (LOGGER.isDebugEnabled()) {
+					LOGGER.atDebug().addArgument(listener).log("READER: acquired lock for listener #{}");
+				}
+				myLock.readUnLock();
+				if (LOGGER.isDebugEnabled()) {
+					LOGGER.atDebug().addArgument(listener).log("READER: released lock for listener #{}");
+				}
+			});
+			threads[i].start();
+		}
+
+		//assertEquals(0, myLockImpl.getReaderCount());
+		assertEquals(1, myLockImpl.getWriterCount());
+		
+		Thread anotherWriterThread = new Thread(() -> {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.atDebug().log("WRITER-#2: try to acquire write lock for another writer thread");
+			}
+			myLock.writeLock();
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.atDebug().log("WRITER-#2: acquired write lock for another writer thread");
+			}
+			sleep(500);
+			myLock.writeUnLock();
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.atDebug().log("WRITER-#2: released write lock for another writer thread");
+			}
+		});
+		anotherWriterThread.start();
+		
+		assertEquals(1, myLockImpl.getWriterCount());
+		
+		myLock.writeLockGuard(() -> {
+			sleep(100);
+		});
+
+		double retVal = myLock.writeLockGuard(() -> {
+			sleep(100);
+			return 2.0;
+		});
+		assertEquals(2.0, retVal);
+
+		myLock.writeLock();
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.atDebug().log("WRITER: after second lock");
+		}
+
+		myLock.writeUnLock();
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.atDebug().log("WRITER: after second un-lock");
+		}
+
+		myLock.writeUnLock();
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.atDebug().log("WRITER: after first un-lock");
+		}
+
+		// recursive read locks
+		myLock.readLock().getDataCount();
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.atDebug().log("READER: after first read lock");
+		}
+
+		myLock.readLock();
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.atDebug().log("READER: after second read lock");
+		}
+
+		myLock.readUnLock();
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.atDebug().log("READER: after second read unlock");
+		}
+
+		myLock.readUnLock();
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.atDebug().log("READER: after first read unlock");
+		}
+		
+		sleep(1000);
+		assertEquals(0, myLockImpl.getReaderCount());
+		assertEquals(0, myLockImpl.getWriterCount());
+		assertTrue(dataSet.isAutoNotification());
+		
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.atDebug().log("finished - testDataSetLock()");
+		}
+	}
+
+	private static void sleep(int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			if (LOGGER.isErrorEnabled()) {
+				LOGGER.atError().setCause(e).log("sleep error");
+			}
+			e.printStackTrace();
+		}
+	}
+}
