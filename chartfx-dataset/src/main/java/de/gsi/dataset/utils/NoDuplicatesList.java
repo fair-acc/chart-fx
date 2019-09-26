e92fe0bafcf438925927b7fb66199cc09d7fd207
rstein <r.steinhagen@gsi.de>
2019-09-26 16:42:13 +0200

    simple reformatting and removing superfluous bracket

--- chartfx-dataset/src/main/java/de/gsi/dataset/utils/NoDuplicatesList.java
+++ chartfx-dataset/src/main/java/de/gsi/dataset/utils/NoDuplicatesList.java
@@ -5,40 +5,38 @@ import java.util.LinkedList;
 
 /**
  * @author unknown
- *
  * @param <E> generics
  */
 public class NoDuplicatesList<E> extends LinkedList<E> {
-	private static final long serialVersionUID = -8547667608571765668L;
+    private static final long serialVersionUID = -8547667608571765668L;
 
-	@Override
-	public boolean add(E e) {
-		if (this.contains(e)) {
-			return false;
-		} else {
-			return super.add(e);
-		}
-	}
+    @Override
+    public boolean add(E e) {
+        if (this.contains(e)) {
+            return false;
+        }
+        return super.add(e);
+    }
 
-	@Override
-	public boolean addAll(Collection<? extends E> collection) {
-		Collection<E> copy = new LinkedList<>(collection);
-		copy.removeAll(this);
-		return super.addAll(copy);
-	}
+    @Override
+    public boolean addAll(Collection<? extends E> collection) {
+        Collection<E> copy = new LinkedList<>(collection);
+        copy.removeAll(this);
+        return super.addAll(copy);
+    }
 
-	@Override
-	public boolean addAll(int index, Collection<? extends E> collection) {
-		Collection<E> copy = new LinkedList<>(collection);
-		copy.removeAll(this);
-		return super.addAll(index, copy);
-	}
+    @Override
+    public boolean addAll(int index, Collection<? extends E> collection) {
+        Collection<E> copy = new LinkedList<>(collection);
+        copy.removeAll(this);
+        return super.addAll(index, copy);
+    }
 
-	@Override
-	public void add(int index, E element) {
-		if (this.contains(element)) {
-			return;
-		}
-		super.add(index, element);
-	}
+    @Override
+    public void add(int index, E element) {
+        if (this.contains(element)) {
+            return;
+        }
+        super.add(index, element);
+    }
 }
