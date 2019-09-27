9f0d19d153f34a49da86e1a85e9648b4c3b8c70a
rstein <r.steinhagen@gsi.de>
2019-09-27 18:22:14 +0200

    updated example to include different flavours how to add data

--- chartfx-samples/src/main/java/de/gsi/chart/samples/SimpleChartSample.java
+++ chartfx-samples/src/main/java/de/gsi/chart/samples/SimpleChartSample.java
@@ -17,33 +17,44 @@ import javafx.stage.Stage;
  * 
  * @author rstein
  */
-public class SimpleChartSample extends Application {  
-	private static final Logger LOGGER = LoggerFactory.getLogger(SimpleChartSample.class);
-    private static final int N_SAMPLES = 100;  // default number of data points
+public class SimpleChartSample extends Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleChartSample.class);
+    private static final int N_SAMPLES = 100; // default number of data points
 
     @Override
     public void start(final Stage primaryStage) {
         final XYChart chart = new XYChart(new DefaultNumericAxis(), new DefaultNumericAxis());
-        chart.getPlugins().add(new Zoomer()); // standard pluging, used in most cases
-        
+        chart.getPlugins().add(new Zoomer()); // standard plugin, useful for most cases
+
         final DoubleDataSet dataSet1 = new DoubleDataSet("data set #1");
-        dataSet1.addListener(evt -> LOGGER.atInfo().log("dataset1 received " + evt.getClass().getSimpleName()));
         final DoubleDataSet dataSet2 = new DoubleDataSet("data set #2");
+
+        // some custom listeners (optional)
+        dataSet1.addListener(evt -> LOGGER.atInfo().log("dataSet1 - event: " + evt.toString()));
+        dataSet2.addListener(evt -> LOGGER.atInfo().log("dataSet2 - event: " + evt.toString()));
+
         // lineChartPlot.getDatasets().add(dataSet1); // for single data set
         chart.getDatasets().addAll(dataSet1, dataSet2); // two data sets
 
         final double[] xValues = new double[N_SAMPLES];
         final double[] yValues1 = new double[N_SAMPLES];
         final double[] yValues2 = new double[N_SAMPLES];
+        //dataSet2.setAutoNotifaction(false); // to suppress auto notification
         for (int n = 0; n < N_SAMPLES; n++) {
-            xValues[n] = n;
-            yValues1[n] = Math.cos(Math.toRadians(10.0 * n));
-            yValues2[n] = Math.sin(Math.toRadians(10.0 * n));
+            final double x = n;
+            final double y1 = Math.cos(Math.toRadians(10.0 * n));
+            final double y2 = Math.sin(Math.toRadians(10.0 * n));
+            xValues[n] = x;
+            yValues1[n] = y1;
+            yValues2[n] = y2;
+            dataSet2.add(n, y2); // style #1 how to set data, notifies re-draw for every 'add'
         }
-        LOGGER.atInfo().log("xRange before = " + dataSet1.getAxisDescription(0).toString());
-        dataSet1.set(xValues, yValues1);
-        dataSet2.set(xValues, yValues2);
-        LOGGER.atInfo().log("xRange after = " + dataSet1.getAxisDescription(0).toString());
+        dataSet1.set(xValues, yValues1); // style #2 how to set data, notifies once per set
+        // dataSet2.setAutoNotifaction(false); // to suppress auto notification
+        // dataSet2.invokeListener(null); // to manually trigger an update
+
+        // alternatively (optional): 
+        // final DoubleDataSet dataSet3 = new DoubleDataSet("data set #1", xValues, yValues1, N_SAMPLES, false);
 
         final Scene scene = new Scene(new StackPane(chart), 800, 600);
         primaryStage.setTitle(getClass().getSimpleName());
