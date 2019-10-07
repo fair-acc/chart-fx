da46c3a78bbf04aafcda5bd0e86326811884bb2a
rstein <r.steinhagen@gsi.de>
2019-10-07 10:52:34 +0200

    updated DataType to synchronise definitions for primitive & boxed types

--- chartfx-dataset/src/main/java/de/gsi/dataset/serializer/DataType.java
+++ chartfx-dataset/src/main/java/de/gsi/dataset/serializer/DataType.java
@@ -1,5 +1,6 @@
 package de.gsi.dataset.serializer;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Enumeration;
 import java.util.List;
@@ -16,62 +17,62 @@ import java.util.Set;
 public enum DataType {
     // @formatter:off
     // start marker
-    START_MARKER             (0,   "bool",          "boolean",             1, Boolean.class, Cat.PRIMITIVE),
+    START_MARKER             (0,   "bool",          "boolean",             1, Cat.SINGLE_VALUE),
     // primitive types
-    BOOL                     (1,   "bool",          "boolean",             1, Boolean.class, Cat.PRIMITIVE),
-    BYTE                     (2,   "byte" ,         "byte",                1, Byte.class, Cat.PRIMITIVE),
-    SHORT                    (3,   "short",         "short",               2, Short.class, Cat.PRIMITIVE),
-    INT                      (4,   "int",           "int",                 4, Integer.class, Cat.PRIMITIVE),
-    LONG                     (5,   "long",          "long",                8, Long.class, Cat.PRIMITIVE),
-    FLOAT                    (6,   "float" ,        "float",               4, Float.class, Cat.PRIMITIVE),
-    DOUBLE                   (7,   "double",        "double",              8, Double.class, Cat.PRIMITIVE),
-    CHAR                     (8,   "char",          "char",                2, Character.class, Cat.PRIMITIVE),
-    STRING                   (9,   "string",        "string",              1, String.class, Cat.PRIMITIVE),
+    BOOL                     (1,   "bool",          "boolean",             1, Cat.SINGLE_VALUE, boolean.class, Boolean.class),
+    BYTE                     (2,   "byte" ,         "byte",                1, Cat.SINGLE_VALUE, byte.class, Byte.class),
+    SHORT                    (3,   "short",         "short",               2, Cat.SINGLE_VALUE, short.class, Short.class),
+    INT                      (4,   "int",           "int",                 4, Cat.SINGLE_VALUE, int.class, Integer.class),
+    LONG                     (5,   "long",          "long",                8, Cat.SINGLE_VALUE, long.class, Long.class),
+    FLOAT                    (6,   "float" ,        "float",               4, Cat.SINGLE_VALUE, float.class, Float.class),
+    DOUBLE                   (7,   "double",        "double",              8, Cat.SINGLE_VALUE, double.class, Double.class),
+    CHAR                     (8,   "char",          "char",                2, Cat.SINGLE_VALUE, char.class, Character.class),
+    STRING                   (9,   "string",        "java.lang.String",    1, Cat.SINGLE_VALUE, String.class),
 
     // array of primitive types
-    BOOL_ARRAY               (101, "bool_array",    "[Z",                  1, Boolean[].class, Cat.ARRAY),
-    BYTE_ARRAY               (102, "byte_array",    "[B",                  1, Byte[].class, Cat.ARRAY),
-    SHORT_ARRAY              (103, "short_array",   "[S",                  2, Short[].class, Cat.ARRAY),
-    INT_ARRAY                (104, "int_array",     "[I",                  4, Integer[].class, Cat.ARRAY),
-    LONG_ARRAY               (105, "long_array",    "[J",                  8, Long[].class, Cat.ARRAY),
-    FLOAT_ARRAY              (106, "float_array",   "[F",                  4, Float[].class, Cat.ARRAY),
-    DOUBLE_ARRAY             (107, "double_array",  "[D",                  8, Double[].class, Cat.ARRAY),
-    CHAR_ARRAY               (108, "char_array",    "[C",                  2, Character[].class, Cat.ARRAY),
-    STRING_ARRAY             (109, "string_array",  "[java.lang.String",   1, String[].class, Cat.ARRAY),
+    BOOL_ARRAY               (101, "bool_array",    "[Z",                  1, Cat.ARRAY, boolean[].class, Boolean[].class),
+    BYTE_ARRAY               (102, "byte_array",    "[B",                  1, Cat.ARRAY, byte[].class, Byte[].class),
+    SHORT_ARRAY              (103, "short_array",   "[S",                  2, Cat.ARRAY, short[].class, Short[].class),
+    INT_ARRAY                (104, "int_array",     "[I",                  4, Cat.ARRAY, int[].class, Integer[].class),
+    LONG_ARRAY               (105, "long_array",    "[J",                  8, Cat.ARRAY, long[].class, Long[].class),
+    FLOAT_ARRAY              (106, "float_array",   "[F",                  4, Cat.ARRAY, float[].class, Float[].class),
+    DOUBLE_ARRAY             (107, "double_array",  "[D",                  8, Cat.ARRAY, double[].class, Double[].class),
+    CHAR_ARRAY               (108, "char_array",    "[C",                  2, Cat.ARRAY, char[].class, Character[].class),
+    STRING_ARRAY             (109, "string_array",  "[java.lang.String",   1, Cat.ARRAY, String[].class),
 
     // complex objects
-    COLLECTION               (200, "collection",     "",                    1, Collection.class, Cat.OBJECT),
-    ENUMERATION              (201, "enumeration",    "",                    1, Enumeration.class, Cat.OBJECT),
-    LIST                     (202, "list",           "",                    1, List.class, Cat.OBJECT),
-    MAP                      (203, "map",            "",                    1, Map.class, Cat.OBJECT),
-    QUEUE                    (204, "queue",          "",                    1, Queue.class, Cat.OBJECT),
-    SET                      (205, "set",            "",                    1, Set.class, Cat.OBJECT),
+    COLLECTION               (200, "collection",     "",                    1, Cat.ARRAY, Collection.class),
+    ENUMERATION              (201, "enumeration",    "",                    1, Cat.COMPLEX_OBJECT, Enumeration.class),
+    LIST                     (202, "list",           "",                    1, Cat.ARRAY, List.class),
+    MAP                      (203, "map",            "",                    1, Cat.ARRAY, Map.class),
+    QUEUE                    (204, "queue",          "",                    1, Cat.ARRAY, Queue.class),
+    SET                      (205, "set",            "",                    1, Cat.ARRAY, Set.class),
 
     // default for future extensions (e.g. moving to short
-    OTHER                    (0xFD, "other",         "",                    1, Object.class, Cat.PRIMITIVE),
+    OTHER                    (0xFD, "other",         "",                    1, Cat.SINGLE_VALUE, Object.class),
     // end marker
-    END_MARKER               (0xFE, "end_marker",    "",                    1, Byte.class, Cat.PRIMITIVE);
+    END_MARKER               (0xFE, "end_marker",    "",                    1, Cat.SINGLE_VALUE);
     // @formatter:on
 
     private final byte byteValue;
     private final long primitiveSize;
     private final String stringValue;
     private final String javaName;
-    private final Class<?> classType;
+    private final List<Class<?>> classTypes;
     private final boolean scalar;
     private final boolean array;
     private final boolean object;
 
     DataType(final int byteValue, final String stringValue, final String javaName, final long primitiveSize,
-            final Class<?> classType, final Cat type) {
+            final Cat type, final Class<?>... classType) {
         this.byteValue = (byte) byteValue;
         this.stringValue = stringValue;
         this.javaName = javaName;
         this.primitiveSize = primitiveSize;
-        this.classType = classType;
-        scalar = type.equals(Cat.PRIMITIVE);
+        this.classTypes = Arrays.asList(classType);
+        scalar = type.equals(Cat.SINGLE_VALUE);
         array = type.equals(Cat.ARRAY);
-        object = type.equals(Cat.OBJECT);
+        object = type.equals(Cat.COMPLEX_OBJECT);
     }
 
     /**
@@ -97,7 +98,7 @@ public enum DataType {
      */
     public static DataType fromClassType(final Class<?> classType) {
         for (final DataType type : DataType.values()) {
-            if (type.getClassType() == classType) {
+            if (type.getClassTypes().contains(classType)) {
                 return type;
             }
         }
@@ -158,8 +159,8 @@ public enum DataType {
      *
      * @return the matching java class type
      */
-    public Class<?> getClassType() {
-        return classType;
+    public List<Class<?>> getClassTypes() {
+        return classTypes;
     }
 
     /**
