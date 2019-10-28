9c36f1ef53314ad03ef34f6b981f9f0d486580c4
rstein <r.steinhagen@gsi.de>
2019-10-28 22:12:35 +0100

    DataViewer clean-up and removal of ControlFX 'Window' dependency
    * N.B. 'Window' caused issues with infinite layoutChildren() loop and
    buggy/incomplete redraw when Window was (un-)minimised... now a simple
    BorderPane.

--- chartfx-chart/src/main/java/de/gsi/chart/viewer/DataViewWindow.java
+++ chartfx-chart/src/main/java/de/gsi/chart/viewer/DataViewWindow.java
@@ -9,10 +9,14 @@ import org.slf4j.LoggerFactory;
 
 import de.gsi.chart.Chart;
 import de.gsi.dataset.utils.ProcessingProfiler;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Bounds;
@@ -20,15 +24,17 @@ import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import jfxtras.labs.scene.control.window.RotateIcon;
-import jfxtras.labs.scene.control.window.Window;
 import jfxtras.labs.scene.control.window.WindowIcon;
 
-public class DataViewWindow extends Window {
+public class DataViewWindow extends BorderPane {
     private static final String CSS_WINDOW_DETACH_ICON = "window-detach-icon";
     private static final Logger LOGGER = LoggerFactory.getLogger(DataViewWindow.class);
     private static final String WINDOW_CSS = "window.css";
@@ -37,17 +43,26 @@ public class DataViewWindow extends Window {
     private static final String CSS_WINDOW_MINIMIZE_ICON2 = "window-minimize-icon2";
     private static final String CSS_WINDOW_RESTORE_ICON = "window-restore-icon";
     private static final String CSS_WINDOW_MAXIMIZE_ICON = "window-maximize-icon";
+    private static final String CSS_WINDOW_TITLE_BAR = "window-titlebar";
+    private static final String CSS_TITLE_LABEL = "title-label";
 
-    private final transient WindowIcon minimizeButton = new WindowIcon();
-    private final transient WindowIcon maximizeRestoreButton = new WindowIcon();
-    private final transient WindowIcon closeButton = new WindowIcon();
-    private final transient WindowIcon detachButton = new WindowIcon();
+    private final StringProperty name = new SimpleStringProperty(this, "name", "");
+    private final HBox leftButtons = new HBox();
+    private final Label label = new Label();
+    private final HBox rightButtons = new HBox();
+    private final HBox windowDecoration = new HBox();
+    private final BooleanProperty minimisedWindow = new SimpleBooleanProperty(this, "minimisedWindow", false);
+    private final BooleanProperty decorationVisible = new SimpleBooleanProperty(this, "windowDecorationVisible", false);
+    private final ObjectProperty<Node> content = new SimpleObjectProperty<>(this, "content");
+
+    private final transient WindowIcon minimizeButton = new MyWindowIcon();
+    private final transient WindowIcon maximizeRestoreButton = new MyWindowIcon();
+    private final transient WindowIcon closeButton = new MyWindowIcon();
+    private final transient WindowIcon detachButton = new MyWindowIcon();
     private final transient ExternalStage dialog = new ExternalStage();
     private transient double xOffset;
     private transient double yOffset;
     private final DataView parentView;
-    private final Node windowContent;
-    private final StringProperty name = new SimpleStringProperty(this, "name");
 
     private final ObjectProperty<Node> graphic = new SimpleObjectProperty<>(this, "graphic");
 
@@ -67,10 +82,9 @@ public class DataViewWindow extends Window {
         if (getParentView().getMinimisedChildren().contains(this)) {
             // this DataViewWindow is minimised
             getParentView().getMinimisedChildren().remove(this);
-            setMinimized(false);
+            setMinimised(false);
             getParentView().getVisibleChildren().add(this);
-            minimizeButton.setDisable(false);
-            requestLayout();
+            //minimizeButton.setDisable(false);
             return;
         }
 
@@ -103,11 +117,11 @@ public class DataViewWindow extends Window {
         }
 
         maximizeRestoreButton.getStyleClass().setAll(CSS_WINDOW_MAXIMIZE_ICON);
-        setMinimized(true);
+        setMinimised(true);
         getParentView().getVisibleChildren().remove(this);
         getParentView().getMinimisedChildren().add(this);
 
-        minimizeButton.setDisable(true);
+        //minimizeButton.setDisable(true);
     };
 
     protected EventHandler<ActionEvent> closeButtonAction = event -> {
@@ -128,8 +142,7 @@ public class DataViewWindow extends Window {
     };
 
     public DataViewWindow(final DataView parent, final String name, final Node content) {
-        super(name);
-        setName(name);
+        super();
         if (parent == null) {
             throw new IllegalArgumentException("parent must not be null");
         }
@@ -137,14 +150,35 @@ public class DataViewWindow extends Window {
         if (content == null) {
             throw new IllegalArgumentException("content must not be null");
         }
-        this.windowContent = content;
+
+        contentProperty().addListener((ch, o, n) -> setCenter(n));
+        label.textProperty().bindBidirectional(nameProperty());
+
+        final Pane spacer1 = new Pane();
+        final Pane spacer2 = new Pane();
+        HBox.setHgrow(spacer1, Priority.ALWAYS);
+        HBox.setHgrow(spacer2, Priority.ALWAYS);
+        HBox.setHgrow(label, Priority.ALWAYS);
+
+        leftButtons.setPrefWidth(USE_COMPUTED_SIZE);
+        rightButtons.setPrefWidth(USE_COMPUTED_SIZE);
+        HBox.setHgrow(leftButtons, Priority.SOMETIMES);
+        HBox.setHgrow(rightButtons, Priority.SOMETIMES);
+
+        windowDecoration.getChildren().addAll(leftButtons, spacer1, label, spacer2, rightButtons);
+        minimisedProperty().addListener((ch, o, n) -> setCenter(n ? null : getContent()));
+        windowDecorationVisible().addListener((ch, o, n) -> setTop(n ? null : windowDecoration));
+        windowDecoration.setPrefHeight(15);
+        windowDecoration.getStyleClass().setAll(CSS_WINDOW_TITLE_BAR);
+        setTop(windowDecoration);
 
         getStyleClass().add(CSS_DATA_VIEW_PANE);
         final String css = DataViewWindow.class.getResource(WINDOW_CSS).toExternalForm();
         getStylesheets().clear();
         getStylesheets().add(css);
-        getLeftIcons().add(new RotateIcon(this));
+        //getLeftIcons().add(new RotateIcon(this));
 
+        label.getStyleClass().setAll(CSS_TITLE_LABEL);
         minimizeButton.getStyleClass().setAll(CSS_WINDOW_MINIMIZE_ICON2);
         maximizeRestoreButton.getStyleClass().setAll(CSS_WINDOW_MAXIMIZE_ICON);
         closeButton.getStyleClass().setAll(CSS_WINDOW_CLOSE_ICON);
@@ -162,7 +196,7 @@ public class DataViewWindow extends Window {
         getLeftIcons().add(detachButton);
 
         setOnMouseReleased(mevt -> {
-            if (isMinimized() || parentView.getMinimisedChildren().contains(this)) {
+            if (isMinimised() || parentView.getMinimisedChildren().contains(this)) {
                 return;
             }
 
@@ -198,15 +232,24 @@ public class DataViewWindow extends Window {
         });
 
         setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-        getContentPane().getChildren().add(this.windowContent);
+        setName(name);
+        setContent(content);
     }
 
-    public final Node getWindowContent() {
-        return windowContent;
+    public ObjectProperty<Node> contentProperty() {
+        return content;
+    }
+
+    public Node getContent() {
+        return contentProperty().get();
     }
 
     public final Node getGraphic() {
         return graphicProperty().get();
+    }
+
+    public ObservableList<Node> getLeftIcons() {
+        return leftButtons.getChildren();
     }
 
     public final String getName() {
@@ -217,30 +260,66 @@ public class DataViewWindow extends Window {
         return parentView;
     }
 
+    public ObservableList<Node> getRightIcons() {
+        return rightButtons.getChildren();
+    }
+
+    public Label getTitleLabel() {
+        return label;
+    }
+
     public final ObjectProperty<Node> graphicProperty() {
         return graphic;
     }
 
-    @Override
-    protected void layoutChildren() {
-        final long start = ProcessingProfiler.getTimeStamp();
-        super.layoutChildren();
-        if (windowContent instanceof Chart) {
-            ProcessingProfiler.getTimeDiff(start,
-                    "pane updated with data set = " + ((Chart) windowContent).getDatasets().get(0).getName());
-        }
+    public boolean isMinimised() {
+        return minimisedProperty().get();
+    }
+
+    public boolean isWindowDecorationVisible() {
+        return windowDecorationVisible().get();
+    }
+
+    public BooleanProperty minimisedProperty() {
+        return minimisedWindow;
     }
 
     public final StringProperty nameProperty() {
         return name;
     }
 
+    public final void setContent(final Node content) {
+        contentProperty().set(content);
+    }
+
     public final void setGraphic(final Node graphic) {
         graphicProperty().set(graphic);
     }
 
+    public void setMinimised(final boolean state) {
+        minimisedProperty().set(state);
+    }
+
     public final void setName(final String name) {
         nameProperty().set(name);
+    }
+
+    public void setWindowDecorationVisible(final boolean state) {
+        windowDecorationVisible().set(state);
+    }
+
+    public BooleanProperty windowDecorationVisible() {
+        return decorationVisible;
+    }
+
+    @Override
+    protected void layoutChildren() {
+        final long start = ProcessingProfiler.getTimeStamp();
+        super.layoutChildren();
+        if (getContent() instanceof Chart) {
+            ProcessingProfiler.getTimeDiff(start,
+                    "pane updated with data set = " + ((Chart) getContent()).getDatasets().get(0).getName());
+        }
     }
 
     protected class ExternalStage extends Stage {
@@ -252,11 +331,10 @@ public class DataViewWindow extends Window {
 
         public ExternalStage() {
             super();
-            titleProperty().bind(DataViewWindow.this.titleProperty());
+            titleProperty().bind(nameProperty());
             final BorderPane dialogContent = new BorderPane();
             final Scene scene = new Scene(dialogContent, 640, 480);
             setScene(scene);
-            
 
             setOnShown(windowEvent -> {
                 if (DataViewWindow.this.equals(parentView.getMaximizedChild())) {
@@ -266,13 +344,13 @@ public class DataViewWindow extends Window {
                 parentView.getVisibleChildren().remove(DataViewWindow.this);
                 parentView.getUndockedChildren().add(DataViewWindow.this);
 
-                getContentPane().getChildren().clear();
-                dialogContent.setCenter(getWindowContent());
+                setCenter(null);
+                dialogContent.setCenter(getContent());
             });
 
             setOnHidden(windowEvent -> {
                 dialogContent.setCenter(null);
-                getContentPane().getChildren().add(getWindowContent());
+                setCenter(getContent());
                 parentView.getUndockedChildren().remove(DataViewWindow.this);
                 parentView.getVisibleChildren().add(DataViewWindow.this);
             });
@@ -321,6 +399,21 @@ public class DataViewWindow extends Window {
             posY = getY();
             LOGGER.atDebug().addArgument(getName()).log("show window '{}'");
             show();
+        }
+    }
+
+    protected class MyWindowIcon extends WindowIcon {
+        MyWindowIcon() {
+            ChangeListener<? super Number> listener = (ch, o, n) -> {
+                final double marginBar = DataViewWindow.this.windowDecoration.getInsets().getTop()
+                        + DataViewWindow.this.windowDecoration.getInsets().getBottom();
+                final double marginButton = this.getInsets().getTop() + this.getInsets().getBottom();
+                final double max = DataViewWindow.this.windowDecoration.getHeight() - marginBar - marginButton;
+                this.setPrefSize(max, max);
+            };
+            this.widthProperty().addListener(listener);
+            this.heightProperty().addListener(listener);
+            DataViewWindow.this.windowDecoration.heightProperty().addListener(listener);
         }
     }
 }
