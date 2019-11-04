849becb7ca8193a3cd2b55a5f611cc63383be09c
Alexander Krimm <A.Krimm@gsi.de>
2019-11-04 17:28:48 +0100

    Remove PackageInfo

--- chartfx-chart/src/main/java/de/gsi/chart/ui/PackageInfo.java
+++
@@ -1,9 +0,0 @@
-package de.gsi.chart.ui;
-
-/**
- * package contains list of JavaFX UI that are not yet part of controlsFX or the Oracle code-base
- * @author rstein
- */
-public class PackageInfo {
-
-}
