90f3b06eec1d9a181642df34a7b9088e5ed82146
Restyled.io <commits@restyled.io>
2020-01-17 12:19:43 +0000

    Restyled by clang-format

--- chartfx-chart/src/test/java/de/gsi/chart/ui/MockListObserver.java
+++ chartfx-chart/src/test/java/de/gsi/chart/ui/MockListObserver.java
@@ -31,6 +31,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
