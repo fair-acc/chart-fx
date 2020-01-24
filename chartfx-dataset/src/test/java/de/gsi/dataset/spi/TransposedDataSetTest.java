946251994f4dc3d2d4ae268455246980be54b846
Alexander Krimm <A.Krimm@gsi.de>
2020-01-24 09:37:42 +0100

    Fix TransposedDataSet for the 3D case
    Also make TransposedDataSetSample asymmetric so wrong indexing crashes
    it and extend unit test to also cover 3D case.

--- chartfx-dataset/src/test/java/de/gsi/dataset/spi/TransposedDataSetTest.java
+++ chartfx-dataset/src/test/java/de/gsi/dataset/spi/TransposedDataSetTest.java
@@ -1,16 +1,21 @@
 package de.gsi.dataset.spi;
 
+import static de.gsi.dataset.DataSet.DIM_X;
+import static de.gsi.dataset.DataSet.DIM_Y;
+import static de.gsi.dataset.DataSet.DIM_Z;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.gsi.dataset.DataSet;
+import de.gsi.dataset.spi.TransposedDataSet.TransposedDataSet3D;
 
 /**
- * @author akrimm
+ * @author Alexander Krimm
  */
 public class TransposedDataSetTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(TransposedDataSetTest.class);
@@ -49,5 +54,57 @@ public class TransposedDataSetTest {
 
         assertEquals(dataSet.get(0, 0), transposed2.get(1, 0));
         assertEquals(dataSet.get(1, 0), transposed2.get(0, 0));
+    }
+
+    @Test
+    public void testWithDataSet3D() {
+        // generate 3D dataset
+        double[] xvalues = new double[] { 1, 2, 3, 4 };
+        double[] yvalues = new double[] { -3, -2, -0, 2, 4 };
+        double[][] zvalues = new double[][] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { -1, -2, -3, -4 },
+                { 1337, 2337, 4242, 2323 } };
+        DoubleDataSet3D dataset = new DoubleDataSet3D("testdataset", xvalues, yvalues, zvalues);
+        // transpose dataset and test indexing
+        TransposedDataSet datasetTransposed = TransposedDataSet.transpose(dataset);
+        assertEquals("testdataset", datasetTransposed.getName());
+        assertEquals(20, datasetTransposed.getDataCount());
+        assertEquals(4, datasetTransposed.getDataCount(DIM_Y));
+        assertEquals(5, datasetTransposed.getDataCount(DIM_X));
+        assertEquals(20, datasetTransposed.getDataCount(DIM_Z));
+        assertEquals(4242, datasetTransposed.get(DIM_Z, 14));
+        assertEquals(6, datasetTransposed.get(DIM_Z, 6));
+        assertEquals(7, datasetTransposed.get(DIM_Z, 11));
+        assertEquals(4242, ((TransposedDataSet3D) datasetTransposed).getZ(4, 2));
+        assertEquals(4, datasetTransposed.get(DIM_Y, 3));
+        assertEquals(4, datasetTransposed.get(DIM_X, 4));
+        assertEquals(3, datasetTransposed.getIndex(DIM_Y, 3.9));
+        assertEquals(2, datasetTransposed.getIndex(DIM_X, -0.5));
+        assertEquals(0, datasetTransposed.getIndex(DIM_Y, -1000));
+        assertEquals(3, datasetTransposed.getIndex(DIM_Y, 1000));
+        assertThrows(IndexOutOfBoundsException.class, () -> datasetTransposed.get(DIM_Y, 4));
+        assertThrows(IndexOutOfBoundsException.class, () -> ((TransposedDataSet3D) datasetTransposed).getZ(5, 1));
+        assertThrows(IndexOutOfBoundsException.class, () -> ((TransposedDataSet3D) datasetTransposed).getZ(0, 4));
+        // untranspose and check indexing again
+        datasetTransposed.setTransposed(false);
+        assertEquals("testdataset", dataset.getName());
+        assertEquals(20, datasetTransposed.getDataCount());
+        assertEquals(4, datasetTransposed.getDataCount(DIM_X));
+        assertEquals(5, datasetTransposed.getDataCount(DIM_Y));
+        assertEquals(20, datasetTransposed.getDataCount(DIM_Z));
+        assertEquals(4242, datasetTransposed.get(DIM_Z, 18));
+        assertEquals(6, datasetTransposed.get(DIM_Z, 5));
+        assertEquals(7, datasetTransposed.get(DIM_Z, 6));
+        assertEquals(4242, ((TransposedDataSet3D) datasetTransposed).getZ(2, 4));
+        assertEquals(4, datasetTransposed.get(DIM_X, 3));
+        assertEquals(4, datasetTransposed.get(DIM_Y, 4));
+        assertEquals(3, datasetTransposed.getIndex(DIM_X, 3.9));
+        assertEquals(2, datasetTransposed.getIndex(DIM_Y, -0.5));
+        assertEquals(0, datasetTransposed.getIndex(DIM_X, -1000));
+        assertEquals(3, datasetTransposed.getIndex(DIM_X, 1000));
+        assertThrows(IndexOutOfBoundsException.class, () -> datasetTransposed.get(DIM_X, 4));
+        assertThrows(IndexOutOfBoundsException.class, () -> ((TransposedDataSet3D) datasetTransposed).getZ(1, 5));
+        assertThrows(IndexOutOfBoundsException.class, () -> ((TransposedDataSet3D) datasetTransposed).getZ(4, 0));
+        // TODO: check event generation. all events should be passed through and every transposition/permutation should
+        // also trigger an event
     }
 }
