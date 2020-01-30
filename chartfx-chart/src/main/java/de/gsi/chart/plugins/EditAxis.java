6251c3e76e036b9e943857f48f7c4c8b46f20d0f
Alexander Krimm <A.Krimm@gsi.de>
2020-01-30 18:49:51 +0100

    Also enable Edit Axis for Plugins added after the Plugin
    Also fixes concurrent access violation on plugin removal

--- chartfx-chart/src/main/java/de/gsi/chart/plugins/EditAxis.java
+++ chartfx-chart/src/main/java/de/gsi/chart/plugins/EditAxis.java
@@ -8,6 +8,7 @@ import java.util.Objects;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.beans.Observable;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ObjectProperty;
@@ -111,7 +112,11 @@ public class EditAxis extends ChartPlugin {
         setAnimated(animated);
 
         chartProperty().addListener((obs, oldChart, newChart) -> {
+            if (oldChart == newChart) {
+                return;
+            }
             if (oldChart != null) {
+                oldChart.getAxes().removeListener(this::axesChangedHandler);
                 removeMouseEventHandlers(oldChart);
             }
             if (newChart != null) {
@@ -131,6 +136,12 @@ public class EditAxis extends ChartPlugin {
 
     private void addMouseEventHandlers(final Chart newChart) {
         newChart.getAxes().forEach(axis -> popUpList.add(new MyPopOver(axis, axis.getSide().isHorizontal())));
+        newChart.getAxes().addListener(this::axesChangedHandler);
+    }
+
+    private void axesChangedHandler(Observable observable) {
+        removeMouseEventHandlers(getChart());
+        addMouseEventHandlers(getChart());
     }
 
     /**
@@ -183,8 +194,8 @@ public class EditAxis extends ChartPlugin {
     private void removeMouseEventHandlers(final Chart oldChart) {
         popUpList.forEach(popOver -> {
             popOver.deregisterMouseEvents();
-            popUpList.remove(popOver);
         });
+        popUpList.clear();
     }
 
     /**
