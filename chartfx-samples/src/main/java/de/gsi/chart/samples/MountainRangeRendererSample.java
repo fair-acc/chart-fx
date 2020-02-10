880fe765a7b02cfcae3efc08d9225d402537b722
rstein <r.steinhagen@gsi.de>
2020-02-10 16:31:54 +0100

    fixes Coverity bug #216369: dereference of null return value

--- chartfx-samples/src/main/java/de/gsi/chart/samples/MountainRangeRendererSample.java
+++ chartfx-samples/src/main/java/de/gsi/chart/samples/MountainRangeRendererSample.java
@@ -90,11 +90,18 @@ public class MountainRangeRendererSample extends Application {
                 new InputStreamReader(ContourChartSample.class.getResourceAsStream("./testdata/image.txt")))) {
             // final BufferedReader reader = new BufferedReader(new InputStreamReader(
             // ContourChartSampleReference.class.getResourceAsStream("./testdata/image.txt")));
-            reader.readLine();
+            String skipLine;
+            if ((skipLine = reader.readLine()) == null) {
+                throw new IllegalStateException("expected non-null line");
+            }
             final String[] x = reader.readLine().split(" ");
-            reader.readLine();
+            if ((skipLine = reader.readLine()) == null) {
+                throw new IllegalStateException("expected non-null line");
+            }
             final String[] y = reader.readLine().split(" ");
-            reader.readLine();
+            if ((skipLine = reader.readLine()) == null) {
+                throw new IllegalStateException("expected non-null line");
+            }
             final String[] z = reader.readLine().split(" ");
 
             final Number[] xValues = MountainRangeRendererSample.toNumberArray(x);
