1faee756991dd976027863ae97bdf96b0d1f0d63
rstein <r.steinhagen@gsi.de>
2020-02-14 19:04:38 +0100

    added simple unit-test for AxisMode enum

---
+++ chartfx-chart/src/test/java/de/gsi/chart/axes/AxisModeTests.java
@@ -0,0 +1,23 @@
+package de.gsi.chart.axes;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author rstein
+ */
+public class AxisModeTests {
+    @Test
+    public void axisModeTests() {
+        assertTrue(AxisMode.X.allowsX());
+        assertFalse(AxisMode.X.allowsY());
+
+        assertFalse(AxisMode.Y.allowsX());
+        assertTrue(AxisMode.Y.allowsY());
+
+        assertTrue(AxisMode.XY.allowsX());
+        assertTrue(AxisMode.XY.allowsY());
+    }
+}
