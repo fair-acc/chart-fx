d53e90a1ee7684d2a4885dafa8647ccce3ffb6d7
Alexander Krimm <A.Krimm@gsi.de>
2020-03-03 19:27:23 +0100

    Add caching and test to apodization function

--- chartfx-math/src/main/java/de/gsi/math/spectra/Apodization.java
+++ chartfx-math/src/main/java/de/gsi/math/spectra/Apodization.java
@@ -1,18 +1,34 @@
 package de.gsi.math.spectra;
 
+import java.util.Map;
+import java.util.WeakHashMap;
+
+import de.gsi.dataset.utils.AssertUtils;
 import de.gsi.math.TMathConstants;
 
 /**
  * implementation of frequenty used apodization (aka. windowing) functions reference:
  * http://en.wikipedia.org/wiki/Apodization_function - feel free to populate this with the other (however also less
  * frequently used) windows
+ * The array for the windows are cached. By default a WeakHashMap is used, but the cache can be replaced e.g by
+ * {@link de.gsi.dataset.utils.Cache} if different caching behaviour is wanted.
  *
  * @author rstein
+ * @author akrimm
  */
 public enum Apodization {
-    Rectangular("rectangular"), Hamming("Hamming"), Hann("von Hann"), HannExp("exp. von Hann"), Blackman("Blackman"),
-    Nuttall("Nuttall"), BlackmanHarris("Blackman-Harris"), BlackmanNuttall("Blackman-Nuttall"), FlatTop("Flat-Top"),
+    Rectangular("rectangular"),
+    Hamming("Hamming"),
+    Hann("von Hann"),
+    HannExp("exp. von Hann"),
+    Blackman("Blackman"),
+    Nuttall("Nuttall"),
+    BlackmanHarris("Blackman-Harris"),
+    BlackmanNuttall("Blackman-Nuttall"),
+    FlatTop("Flat-Top"),
     Exponential("exponential");
+
+    private static Map<ApodizationArrayDescription, double[]> windowCache = new WeakHashMap<>();
 
     private String fname;
 
@@ -24,13 +40,59 @@ public enum Apodization {
      * computes and returns the value of the apodization function for a given window index
      *
      * @param i index within window
-     * @param n length of window @return, the apodization value
+     * @param n length of window
      * @return value of the apodization function
      */
     public double getIndex(final int i, final int n) {
+        return getWindow(n)[i];
+    }
+
+    /**
+     * computes and returns the value of the apodization function for a given window index
+     *
+     * @param i index within window
+     * @param n length of window
+     * @param m additional window (typ. bandwidth, power, etc)
+     * @return value of the apodization function
+     */
+    public double getIndex(final int i, final int n, final double m) {
         switch (this) {
+        case Hamming:
+            return getIndex(i, n);
+        case Hann:
+            return getIndex(i, n);
+        case HannExp: {
+            final double a = TMathConstants.TwoPi() / (n - 1);
+            return 1.0 * TMathConstants.Power(TMathConstants.Sin(a * i), m);
+        }
+        case Nuttall:
+            return getIndex(i, n);
+        case BlackmanHarris:
+            return getIndex(i, n);
+        case BlackmanNuttall:
+            return getIndex(i, n);
+        case FlatTop:
+            return getIndex(i, n);
+        case Blackman:
+            return getIndex(i, n);
+        case Exponential:
+            return getIndex(i, n);
+        default:
         case Rectangular:
-            return 1.0;
+            return getIndex(i, n);
+        }
+    }
+
+    /**
+     * Directly computes a single window value. If the whole window is required multiple times
+     * it will be faster to use the cached version {@link #getIndex(int, int)} provides.
+     * 
+     * @param i index within the window
+     * @param n length of the window
+     * @return value of the apodization function
+     */
+    public double getIndexUncached(final int i, final int n) {
+        switch (this) {
         case Hamming: {
             // final double a = TMath.TwoPi()/(n-1);
             // the following Hamming window definition is more common
@@ -74,43 +136,7 @@ public enum Apodization {
             return TMathConstants.Exp(i / a) / exp0;
         }
         default:
-            return 1.0;
-        }
-    }
-
-    /**
-     * computes and returns the value of the apodization function for a given window index
-     *
-     * @param i index within window
-     * @param n length of window
-     * @param m additional window (typ. bandwidth, power, etc) @return, the apodization value
-     * @return value of the apodization function
-     */
-    public double getIndex(final int i, final int n, final double m) {
-        switch (this) {
         case Rectangular:
-            return getIndex(i, n);
-        case Hamming:
-            return getIndex(i, n);
-        case Hann:
-            return getIndex(i, n);
-        case HannExp: {
-            final double a = TMathConstants.TwoPi() / (n - 1);
-            return 1.0 * TMathConstants.Power(TMathConstants.Sin(a * i), m);
-        }
-        case Nuttall:
-            return getIndex(i, n);
-        case BlackmanHarris:
-            return getIndex(i, n);
-        case BlackmanNuttall:
-            return getIndex(i, n);
-        case FlatTop:
-            return getIndex(i, n);
-        case Blackman:
-            return getIndex(i, n);
-        case Exponential:
-            return getIndex(i, n);
-        default:
             return 1.0;
         }
     }
@@ -119,14 +145,103 @@ public enum Apodization {
         return fname;
     }
 
-    public static void apodize(final double[] data, final Apodization function) {
+    /**
+     * Returns the window for a
+     * 
+     * @param n window size
+     * @return a cached array containing the requested window
+     */
+    public double[] getWindow(final int n) {
+        return windowCache.computeIfAbsent(new ApodizationArrayDescription(this, n), desc -> {
+            final double[] window = new double[desc.length];
+            for (int i = 0; i < desc.length; i++) {
+                window[i] = getIndexUncached(i, desc.length);
+            }
+            return window;
+        });
+    }
+
+    /**
+     * Applies the given apodization window to the given array (in-place)
+     * 
+     * @param data input data
+     */
+    public void apodize(final double[] data) {
+        final double[] window = getWindow(data.length);
         for (int i = 0; i < data.length; i++) {
-            final double val = function.getIndex(i, data.length);
-            data[i] = data[i] * val;
+            data[i] = data[i] * window[i];
         }
     }
 
-    public static void main(final String argv[]) {
-        System.out.println(Apodization.Hann.getName() + " index (240,1024) = " + Apodization.Hann.getIndex(240, 1024));
+    /**
+     * Applies the given apodization window to the given array (in-place)
+     * 
+     * @param data input data
+     * @param function the apodization function to use
+     */
+    public static void apodize(final double[] data, final Apodization function) {
+        function.apodize(data);
+    }
+
+    /**
+     * @return the global cache for the apodization windows
+     */
+    public static Map<ApodizationArrayDescription, double[]> getWindowCache() {
+        return windowCache;
+    }
+
+    /**
+     * Changes the map used to cache the apodization windows.
+     * The map should implement some sort of caching behaviour e.g
+     * <ul>
+     * <li>{@link java.util.WeakHashMap} {@code<ApodizationArrayDescription, double[]>} invalidates under memory
+     * pressure (The default)
+     * <li>{@link de.gsi.dataset.utils.Cache} limits retention time and number of cached entries
+     * <li>custom implementation
+     * </ul>
+     * 
+     * @param windowCache A map to use as a cache for the apodization windows
+     */
+    public static void setWindowCache(Map<ApodizationArrayDescription, double[]> windowCache) {
+        Apodization.windowCache = windowCache;
+    }
+
+    /**
+     * private class identifying a specific buffer array, used as a key for the array cache
+     */
+    protected static class ApodizationArrayDescription {
+        final protected Apodization apodization;
+        final protected int length;
+
+        /**
+         * @param apodization
+         * @param length
+         */
+        public ApodizationArrayDescription(Apodization apodization, int length) {
+            AssertUtils.notNull("apodization", apodization);
+            this.apodization = apodization;
+            this.length = length;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (!(obj instanceof ApodizationArrayDescription)) {
+                return false;
+            }
+            ApodizationArrayDescription other = (ApodizationArrayDescription) obj;
+            return (apodization == other.apodization && length == other.length);
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + apodization.hashCode();
+            result = prime * result + length;
+            return result;
+        }
     }
 }
