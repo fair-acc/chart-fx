1f24188215706bc8a8642cadfc89ae415cfd4bf7
Alexander Krimm <A.Krimm@gsi.de>
2020-03-16 13:27:23 +0100

    Fix setupGitHooks.sh relative symlinks.

--- setupGitHooks.sh
+++ setupGitHooks.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 # git config core.hooksPath $PWD/config/hooks
 # safe fall-back for older git versions
-ln -s ./config/hooks/pre* ./.git/hooks/
+ln -s -r ./config/hooks/pre* -t ./.git/hooks/
