6a89cceca3e1ceead4386bfcb15c14583b55fdde
rstein <r.steinhagen@gsi.de>
2020-03-28 23:22:32 +0100

    added SelectiveJavaFxInterceptor + refactored existing JavaFxInterceptor
    * added new @TestFx annotation and corresponding
    SelectiveJavaFxInterceptor extension.
    Usage example
    a) '@ExtendWith(JavaFxInterceptor.class)' before test class:
     - functions with @TestFx and/or @Test are executed in JavaFX thread
    b) '@ExtendWith(SelectiveJavaFxInterceptor.class)' before test class:
     - only functions with @TestFx are executed in JavaFX thread
     - functions with @Test are executed in JUnit's standard thread pool
    
    
    hopefully the new functionality and refactoring should make the testing
    classes/readability more consistent w.r.t. the different use cases.
    
    Enjoy!

---
+++ chartfx-chart/src/test/java/de/gsi/chart/ui/utils/TestFx.java
@@ -0,0 +1,27 @@
+package de.gsi.chart.ui.utils;
+
+import static org.apiguardian.api.API.Status.STABLE;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.apiguardian.api.API;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+/**
+ * extension of JUnits @Test annotation to indicate that a particular test should be explicitly executed within the JavaFX thread. 
+ * See also {@link de.gsi.chart.ui.utils.JavaFXInterceptorUtils.SelectiveJavaFxInterceptor }
+ * @author rstein
+ *
+ */
+@Target({ ElementType.ANNOTATION_TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@API(status = STABLE, since = "5.0")
+@Test
+@Tag("TestWithinJavaFX")
+public @interface TestFx { }
