22253d0236449bb9f5aca3e55b56aaf0a51499fe
Ralph J. Steinhagen <46007894+RalphSteinhagen@users.noreply.github.com>
2020-03-30 10:14:28 +0200

    added SelectiveJavaFxInterceptor + refactored existing JavaFxInterceptor (#138)
    * added new @TestFx annotation and corresponding
    SelectiveJavaFxInterceptor extension.
    Usage example
    a) '@ExtendWith(JavaFxInterceptor.class)' before test class:
     - functions with @TestFx and/or @Test are executed in JavaFX thread
    b) '@ExtendWith(SelectiveJavaFxInterceptor.class)' before test class:
     - only functions with @TestFx are executed in JavaFX thread
     - functions with @Test are executed in JUnit's standard thread pool
    
    
    hopefully the new functionality and refactoring should make the testing
    classes/readability more consistent w.r.t. the different use cases.
    
    Enjoy!

---
+++ chartfx-chart/src/test/java/de/gsi/chart/ui/utils/JavaFxInterceptorTests.java
@@ -0,0 +1,52 @@
+package de.gsi.chart.ui.utils;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import de.gsi.chart.ui.utils.JavaFXInterceptorUtils.JavaFxInterceptor;
+
+/**
+ * Tests for {@link de.gsi.chart.ui.utils.JavaFXInterceptorUtils.JavaFxInterceptor}.
+ *
+ * @author rstein
+ *
+ */
+@ExtendWith(ApplicationExtension.class)
+@ExtendWith(JavaFxInterceptor.class)
+public class JavaFxInterceptorTests {
+    @Start
+    public void start(final Stage stage) {
+        // usual FX initialisation
+        // ...
+        assertTrue(Platform.isFxApplicationThread());
+        stage.setScene(new Scene(new StackPane(), 100, 100));
+        stage.show();
+    }
+
+    @TestFx // note: this is equivalent to {@code @Test} when using {@code @ExtendWith(JavaFxInterceptor.class)}
+    public void testJavaFxThreadSafety() {
+        // verifies that this test is indeed executed in the JavaFX thread
+        assertTrue(Platform.isFxApplicationThread());
+
+        // perform regular assertion tests within the JavaFX thread
+        // ...
+    }
+
+    @Test // also executed in JavaFX thread, for different behaviour use: {@code @ExtendWith(SelectiveJavaFxInterceptor.class)
+    public void testNonJavaFx() {
+        // verifies that this test is indeed executed in the JavaFX thread
+        assertTrue(Platform.isFxApplicationThread());
+
+        // perform also the regular assertion tests within the JavaFX thread
+        // ...
+    }
+}
---
+++ chartfx-chart/src/test/java/de/gsi/chart/ui/utils/SelectiveJavaFxInterceptorTests.java
@@ -0,0 +1,52 @@
+package de.gsi.chart.ui.utils;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import de.gsi.chart.ui.utils.JavaFXInterceptorUtils.SelectiveJavaFxInterceptor;
+
+/**
+ * Tests for {@link de.gsi.chart.ui.utils.JavaFXInterceptorUtils.SelectiveJavaFxInterceptor}.
+ *
+ * @author rstein
+ */
+@ExtendWith(ApplicationExtension.class)
+@ExtendWith(SelectiveJavaFxInterceptor.class)
+public class SelectiveJavaFxInterceptorTests {
+    @Start
+    public void start(final Stage stage) {
+        // usual FX initialisation
+        // ...
+        assertTrue(Platform.isFxApplicationThread());
+        stage.setScene(new Scene(new StackPane(), 100, 100));
+        stage.show();
+    }
+
+    @TestFx // forces execution in JavaFX thread
+    public void testJavaFxThreadSafety() {
+        // verifies that this test is indeed executed in the JavaFX thread
+        assertTrue(Platform.isFxApplicationThread());
+
+        // perform the regular JavaFX thread safe assertion tests
+        // ...
+    }
+
+    @Test // explicitly not executed in JavaFX thread; for different behaviour use: {@code @ExtendWith(JavaFxInterceptor.class)
+    public void testNonJavaFx() {
+        // verifies that this test is not executed within the JavaFX thread
+        assertFalse(Platform.isFxApplicationThread());
+
+        // perform the regular non-JavaFX thread-related assertion tests
+        // ...
+    }
+}
