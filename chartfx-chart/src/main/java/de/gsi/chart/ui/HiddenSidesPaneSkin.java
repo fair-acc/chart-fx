af070a291d46e78b0312c5f6348fbb0a3242bc10
rstein <r.steinhagen@gsi.de>
2020-04-01 12:02:25 +0200

    fix: respect requested StackPane Pos alignment requirements
    N.B. plus some minor semantic and PMD-related bug fixes.

--- chartfx-chart/src/main/java/de/gsi/chart/ui/HiddenSidesPaneSkin.java
+++ chartfx-chart/src/main/java/de/gsi/chart/ui/HiddenSidesPaneSkin.java
@@ -27,7 +27,6 @@ package de.gsi.chart.ui;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import de.gsi.dataset.serializer.spi.ClassDescriptions;
 import javafx.animation.Animation.Status;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
@@ -36,6 +35,7 @@ import javafx.beans.InvalidationListener;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.geometry.Side;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -44,6 +44,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
+
+import de.gsi.dataset.serializer.spi.ClassDescriptions;
 
 public class HiddenSidesPaneSkin extends SkinBase<HiddenSidesPane> {
     private final StackPane stackPane;
@@ -66,6 +68,7 @@ public class HiddenSidesPaneSkin extends SkinBase<HiddenSidesPane> {
         };
 
         stackPane = new StackPane();
+        stackPane.setId("HiddenSidesPaneSkin::StackPane");
         getChildren().add(stackPane);
         updateStackPane();
 
@@ -79,10 +82,12 @@ public class HiddenSidesPaneSkin extends SkinBase<HiddenSidesPane> {
         pane.addEventFilter(MouseEvent.MOUSE_MOVED, event -> {
             if (isMouseEnabled() && getSkinnable().getPinnedSide() == null) {
                 final Side side = getSide(event);
-                if (side != null) {
+                if (side == null) {
+                    if (isMouseMovedOutsideSides(event)) {
+                        hide();
+                    }
+                } else {
                     show(side);
-                } else if (isMouseMovedOutsideSides(event)) {
-                    hide();
                 }
             }
         });
@@ -96,16 +101,16 @@ public class HiddenSidesPaneSkin extends SkinBase<HiddenSidesPane> {
 
             if (isMouseEnabled() && getSkinnable().getPinnedSide() == null) {
                 final Side side = getSide(event);
-                if (side != null) {
-                    show(side);
-                } else {
+                if (side == null) {
                     hide();
+                } else {
+                    show(side);
                 }
             }
         });
 
         for (final Side side : Side.values()) {
-            visibility[side.ordinal()] = new SimpleDoubleProperty(0);
+            visibility[side.ordinal()] = new SimpleDoubleProperty(0); // NOPMD initial allocation, not recurrent
             visibility[side.ordinal()].addListener(observable -> getSkinnable().requestLayout());
         }
 
@@ -125,21 +130,35 @@ public class HiddenSidesPaneSkin extends SkinBase<HiddenSidesPane> {
         getSkinnable().setClip(clip);
     }
 
-    protected Node getSidePane(Side side) {
-        if (getSkinnable() == null) {
-            return null;
+    private double getPrefAlignmentH(final Node node, final double contentWidth) {
+        final Pos alignment = StackPane.getAlignment(node);
+        if (alignment == null) {
+            return 0.0;
         }
-        switch (side) {
-        case BOTTOM:
-            return getSkinnable().getBottom();
-        case LEFT:
-            return getSkinnable().getLeft();
+        switch (alignment.getHpos()) {
         case RIGHT:
-            return getSkinnable().getRight();
-        case TOP:
-            return getSkinnable().getTop();
+            return contentWidth - node.getLayoutBounds().getWidth();
+        case CENTER:
+            return 0.5 * (contentWidth - node.getLayoutBounds().getWidth());
+        case LEFT:
         default:
-            return null;
+            return 0.0;
+        }
+    }
+
+    private double getPrefAlignmentV(final Node node, final double contentHeight) {
+        final Pos alignment = StackPane.getAlignment(node);
+        if (alignment == null) {
+            return 0.0;
+        }
+        switch (alignment.getVpos()) {
+        case BOTTOM:
+            return contentHeight - node.getLayoutBounds().getHeight();
+        case CENTER:
+            return 0.5 * (contentHeight - node.getLayoutBounds().getHeight());
+        case TOP:
+        default:
+            return 0.0;
         }
     }
 
@@ -159,6 +178,32 @@ public class HiddenSidesPaneSkin extends SkinBase<HiddenSidesPane> {
         }
 
         return null;
+    }
+
+    /**
+     * @return
+     */
+    private boolean hasShowingChild(Node n) {
+        if (n == null) {
+            return false;
+        }
+        if (n.isHover()) {
+            lastHideBlockingNode = n;
+            return true;
+        }
+        try {
+            Method showingMethod = ClassDescriptions.getMethod(n.getClass(), "isShowing");
+            if (showingMethod != null && (Boolean) showingMethod.invoke(n)) {
+                lastHideBlockingNode = n;
+                return true;
+            }
+        } catch (SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            // do nothing
+        }
+        if (n instanceof Parent) {
+            return ((Parent) n).getChildrenUnmodifiable().stream().anyMatch(this::hasShowingChild);
+        }
+        return false;
     }
 
     private void hide() {
@@ -212,10 +257,8 @@ public class HiddenSidesPaneSkin extends SkinBase<HiddenSidesPane> {
             keyValues[side.ordinal()] = new KeyValue(visibility[side.ordinal()], 0);
         }
 
-        final Duration delay = getSkinnable().getAnimationDelay() != null ? getSkinnable().getAnimationDelay()
-                : Duration.millis(300);
-        final Duration duration = getSkinnable().getAnimationDuration() != null ? getSkinnable().getAnimationDuration()
-                : Duration.millis(200);
+        final Duration delay = getSkinnable().getAnimationDelay() == null ? Duration.millis(300) : getSkinnable().getAnimationDelay();
+        final Duration duration = getSkinnable().getAnimationDuration() == null ? Duration.millis(300) : getSkinnable().getAnimationDuration();
 
         final KeyFrame keyFrame = new KeyFrame(duration, keyValues);
         hideTimeline = new Timeline(keyFrame);
@@ -223,98 +266,15 @@ public class HiddenSidesPaneSkin extends SkinBase<HiddenSidesPane> {
         hideTimeline.play();
     }
 
-    /**
-     * @return
-     */
-    private boolean hasShowingChild(Node n) {
-        if (n == null) {
-            return false;
-        }
-        if (n.isHover()) {
-            lastHideBlockingNode = n;
-            return true;
-        }
-        try {
-            Method isShowingMethod = ClassDescriptions.getMethod(n.getClass(), "isShowing");
-            if (isShowingMethod != null && (Boolean) isShowingMethod.invoke(n)) {
-                lastHideBlockingNode = n;
-                return true;
-            }
-        } catch (SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-            // do nothing
-        }
-        if (n instanceof Parent) {
-            return ((Parent) n).getChildrenUnmodifiable().stream().anyMatch(child -> hasShowingChild(child));
-        }
-        return false;
-    }
-
     private boolean isMouseEnabled() {
         return getSkinnable().getTriggerDistance() > 0;
     }
 
     private boolean isMouseMovedOutsideSides(MouseEvent event) {
-        return !(getSkinnable().getLeft() != null
-                && getSkinnable().getLeft().getBoundsInParent().contains(event.getX(), event.getY()))
-                && (!(getSkinnable().getTop() != null
-                        && getSkinnable().getTop().getBoundsInParent().contains(event.getX(), event.getY()))
-                        && (!(getSkinnable().getRight() != null
-                                && getSkinnable().getRight().getBoundsInParent().contains(event.getX(), event.getY()))
-                                && !(getSkinnable().getBottom() != null && getSkinnable().getBottom()
-                                        .getBoundsInParent().contains(event.getX(), event.getY()))));
-    }
-
-    @Override
-    protected void layoutChildren(double contentX, double contentY, double contentWidth, double contentHeight) {
-
-        // System.out.println("HiddenSidesPaneSkin.layoutChildren: "+contentX+" "+contentY+" "+contentWidth+"
-        // "+contentHeight);
-        /*
-         * Layout the stackpane in a normal way (equals "lay out the content node", the only managed node)
-         */
-        super.layoutChildren(contentX, contentY, contentWidth, contentHeight);
-
-        /*
-         * Layout the content node excplicitly: TODO: should this not come last and take visible side panes into
-         * account???
-         */
-        if (getSkinnable().getContent() != null) {
-            getSkinnable().getContent().resizeRelocate(contentX, contentY, contentWidth, contentHeight);
-        }
-
-        // layout the unmanaged side nodes
-
-        final Node bottom = getSkinnable().getBottom();
-        if (bottom != null) {
-            final double prefHeight = bottom.prefHeight(-1);
-            final double offset = prefHeight * visibility[Side.BOTTOM.ordinal()].get();
-            bottom.resizeRelocate(contentX, contentY + contentHeight - offset, contentWidth, prefHeight);
-            bottom.setVisible(visibility[Side.BOTTOM.ordinal()].get() > 0);
-        }
-
-        final Node left = getSkinnable().getLeft();
-        if (left != null) {
-            final double prefWidth = left.prefWidth(-1);
-            final double offset = prefWidth * visibility[Side.LEFT.ordinal()].get();
-            left.resizeRelocate(contentX - (prefWidth - offset), contentY, prefWidth, contentHeight);
-            left.setVisible(visibility[Side.LEFT.ordinal()].get() > 0);
-        }
-
-        final Node right = getSkinnable().getRight();
-        if (right != null) {
-            final double prefWidth = right.prefWidth(-1);
-            final double offset = prefWidth * visibility[Side.RIGHT.ordinal()].get();
-            right.resizeRelocate(contentX + contentWidth - offset, contentY, prefWidth, contentHeight);
-            right.setVisible(visibility[Side.RIGHT.ordinal()].get() > 0);
-        }
-
-        final Node top = getSkinnable().getTop();
-        if (top != null) {
-            final double prefHeight = top.prefHeight(-1);
-            final double offset = prefHeight * visibility[Side.TOP.ordinal()].get();
-            top.resizeRelocate(contentX, contentY - (prefHeight - offset), contentWidth, prefHeight);
-            top.setVisible(visibility[Side.TOP.ordinal()].get() > 0);
-        }
+        return !(getSkinnable().getLeft() != null && getSkinnable().getLeft().getBoundsInParent().contains(event.getX(), event.getY()))
+                && (!(getSkinnable().getTop() != null && getSkinnable().getTop().getBoundsInParent().contains(event.getX(), event.getY()))
+                        && (!(getSkinnable().getRight() != null && getSkinnable().getRight().getBoundsInParent().contains(event.getX(), event.getY()))
+                                && !(getSkinnable().getBottom() != null && getSkinnable().getBottom().getBoundsInParent().contains(event.getX(), event.getY()))));
     }
 
     private void show(Side side) {
@@ -331,10 +291,8 @@ public class HiddenSidesPaneSkin extends SkinBase<HiddenSidesPane> {
             keyValues[s.ordinal()] = new KeyValue(visibility[s.ordinal()], s.equals(side) ? 1 : 0);
         }
 
-        final Duration delay = getSkinnable().getAnimationDelay() != null ? getSkinnable().getAnimationDelay()
-                : Duration.millis(300);
-        final Duration duration = getSkinnable().getAnimationDuration() != null ? getSkinnable().getAnimationDuration()
-                : Duration.millis(200);
+        final Duration delay = getSkinnable().getAnimationDelay() == null ? Duration.millis(300) : getSkinnable().getAnimationDelay();
+        final Duration duration = getSkinnable().getAnimationDuration() == null ? Duration.millis(200) : getSkinnable().getAnimationDuration();
 
         final KeyFrame keyFrame = new KeyFrame(duration, keyValues);
         showTimeline = new Timeline(keyFrame);
@@ -371,6 +329,77 @@ public class HiddenSidesPaneSkin extends SkinBase<HiddenSidesPane> {
             getSkinnable().getLeft().setManaged(false);
             getSkinnable().getLeft().removeEventFilter(MouseEvent.MOUSE_EXITED, exitedHandler);
             getSkinnable().getLeft().addEventFilter(MouseEvent.MOUSE_EXITED, exitedHandler);
+        }
+    }
+
+    protected Node getSidePane(Side side) {
+        if (getSkinnable() == null) {
+            return null;
+        }
+        switch (side) {
+        case BOTTOM:
+            return getSkinnable().getBottom();
+        case LEFT:
+            return getSkinnable().getLeft();
+        case RIGHT:
+            return getSkinnable().getRight();
+        case TOP:
+            return getSkinnable().getTop();
+        default:
+            return null;
+        }
+    }
+
+    @Override
+    protected void layoutChildren(double contentX, double contentY, double contentWidth, double contentHeight) {
+        /*
+         * Layout the StackPane in a normal way (equals "lay out the content node", the only managed node)
+         */
+        super.layoutChildren(contentX, contentY, contentWidth, contentHeight);
+
+        /*
+         * Layout the centre node (content) explicitly. This is unaffected by and/or covered by the potentially open/visible SidePanes.
+         */
+        if (getSkinnable().getContent() != null) {
+            getSkinnable().getContent().resizeRelocate(contentX, contentY, contentWidth, contentHeight);
+        }
+
+        // layout the unmanaged side nodes
+
+        final Node bottom = getSkinnable().getBottom();
+        if (bottom != null) {
+            final double prefHeight = bottom.prefHeight(-1);
+            final double offset = prefHeight * visibility[Side.BOTTOM.ordinal()].get();
+            final double offsetX = getPrefAlignmentH(bottom, contentWidth);
+            bottom.resizeRelocate(contentX + offsetX, contentY + contentHeight - offset, contentWidth, prefHeight);
+            bottom.setVisible(visibility[Side.BOTTOM.ordinal()].get() > 0);
+        }
+
+        final Node left = getSkinnable().getLeft();
+        if (left != null) {
+            final double prefWidth = left.prefWidth(-1);
+            final double offset = prefWidth * visibility[Side.LEFT.ordinal()].get();
+            final double offsetY = getPrefAlignmentV(left, contentHeight);
+            left.resizeRelocate(contentX - (prefWidth - offset), contentY + offsetY, prefWidth, contentHeight);
+            left.setVisible(visibility[Side.LEFT.ordinal()].get() > 0);
+        }
+
+        final Node right = getSkinnable().getRight();
+        if (right != null) {
+            final double prefWidth = right.prefWidth(-1);
+            final double offset = prefWidth * visibility[Side.RIGHT.ordinal()].get();
+            final double offsetY = getPrefAlignmentV(right, contentHeight);
+            right.resizeRelocate(contentX + contentWidth - offset, contentY + offsetY, prefWidth, contentHeight);
+            right.setVisible(visibility[Side.RIGHT.ordinal()].get() > 0);
+        }
+
+        final Node top = getSkinnable().getTop();
+        if (top != null) {
+            final double prefHeight = top.prefHeight(-1);
+            final double offset = prefHeight * visibility[Side.TOP.ordinal()].get();
+            final double offsetX = getPrefAlignmentH(top, contentWidth);
+            top.resizeRelocate(contentX + offsetX, contentY - (prefHeight - offset), contentWidth, prefHeight);
+            top.setVisible(visibility[Side.TOP.ordinal()].get() > 0);
         }
     }
 }
