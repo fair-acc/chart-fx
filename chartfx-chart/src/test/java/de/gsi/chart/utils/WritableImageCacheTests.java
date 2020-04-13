1752b4b4a094f1bed54ac8fe9ab9ba7afb35d5d1
rstein <r.steinhagen@gsi.de>
2020-04-13 19:20:08 +0200

    new CacheCollections, ByteArrayCache & WritableImageCache classes
    N.B. Implements cache collection to minimise memory re-allocation and in
    turn garbage collection for short-lived data storage objects. Other data
    storage contained can be similarly derived from CacheCollections. See
    ByteArrayCache for inspiration

---
+++ chartfx-chart/src/test/java/de/gsi/chart/utils/WritableImageCacheTests.java
@@ -0,0 +1,172 @@
+package de.gsi.chart.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javafx.scene.image.WritableImage;
+
+/**
+ * 
+ * Tests implementation of {@link de.gsi.chart.utils.WritableImageCache} as well as implicitly {@link de.gsi.dataset.utils.CacheCollection}.
+ * 
+ * @author rstein
+ *
+ *         N.B. to run manually: javac WritableImageCacheTests.java java -Xms256m -Xmx256m WritableImageCacheTests
+ */
+public class WritableImageCacheTests {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WritableImageCacheTests.class);
+    private static final int N_ITERATIONS = 1000;
+    private static final int N_INITIAL_CACHE_OBJECTS = 10;
+    private static final int N_IMAGE_WIDTH = 200;
+    private static final int N_IMAGE_HEIGHT = 100;
+
+    @Test
+    public void testBasicFunction() {
+        final WritableImageCache cache = new WritableImageCache();
+        assertEquals(0, cache.size(), "initial cache size");
+
+        for (int i = 0; i < N_INITIAL_CACHE_OBJECTS; i++) {
+            cache.add(new WritableImage(N_IMAGE_WIDTH, N_IMAGE_HEIGHT));
+        }
+        assertEquals(N_INITIAL_CACHE_OBJECTS, cache.size(), "allocated dummy arrays -- N.B. if this fails the testing environment likely has not have enough memory");
+
+        // force release of SoftReferences (should work fine starting with > JDK11, JDK
+        // < may need VM parameter: `-XX:SoftRefLRUPolicyMSPerMB=0`
+        forceMemoryShortage();
+        assertEquals(0, cache.size(), "zero-ing memory and SoftReferenes");
+
+        cache.add(new WritableImage(N_IMAGE_WIDTH-10, N_IMAGE_HEIGHT)); // to small
+        cache.add(new WritableImage(N_IMAGE_WIDTH, N_IMAGE_HEIGHT -10)); // to small
+        cache.add(new WritableImage(N_IMAGE_WIDTH, N_IMAGE_HEIGHT)); // goldy locks, right size
+        cache.add(new WritableImage(N_IMAGE_WIDTH+10, N_IMAGE_HEIGHT)); // to large
+        assertEquals(4, cache.size());
+
+        final WritableImage testImage = cache.getImage(N_IMAGE_WIDTH, N_IMAGE_HEIGHT);
+        assertEquals(N_IMAGE_WIDTH, (int)testImage.getWidth());
+        assertEquals(N_IMAGE_HEIGHT, (int)testImage.getHeight());
+        assertEquals(3, cache.size());
+
+        cache.clear();
+        assertEquals(0, cache.size());
+
+        // prevent adding twice
+        assertFalse(cache.contains(testImage));
+        assertTrue(cache.add(testImage));
+        assertTrue(cache.contains(testImage));
+        assertFalse(cache.add(testImage));
+
+        // check null safe-guard
+        assertFalse(cache.add(null));
+        assertFalse(cache.contains(null));
+        assertFalse(cache.remove(null));
+
+        // remove non-existent/unknown image
+        final WritableImage unknownImage = new WritableImage(N_IMAGE_WIDTH, N_IMAGE_HEIGHT);
+        assertFalse(cache.remove(unknownImage));
+    }
+
+    @Test
+    public void testIterators() {
+        final WritableImageCache cache = new WritableImageCache();
+        assertEquals(0, cache.size(), "initial cache size");
+
+        for (int i = 0; i < N_INITIAL_CACHE_OBJECTS; i++) {
+            cache.add(new WritableImage(N_IMAGE_WIDTH, N_IMAGE_HEIGHT));
+        }
+        assertEquals(N_INITIAL_CACHE_OBJECTS, cache.size(), "allocated dummy arrays -- N.B. if this fails the testing environment likely has not have enough memory");
+
+        final java.util.Iterator<WritableImage> iter = cache.iterator();
+        int count = 0;
+        while (iter.hasNext()) {
+            final WritableImage element = iter.next();
+            if (count == 0 || element == null) {
+                iter.remove();
+            }
+            count++;
+        }
+        assertEquals(N_INITIAL_CACHE_OBJECTS, count);
+    }
+
+    private static void forceMemoryShortage() {
+        boolean run = true;
+        List<byte[]> strongReference = Collections.synchronizedList(new ArrayList<>(100000));
+
+        while (run) {
+            try {
+                strongReference.add(new byte[10_000_000]);
+            } catch (final OutOfMemoryError e) {
+                run = false;
+            }
+        }
+
+        // to be garbage collected
+        strongReference = null;
+        Runtime.getRuntime().gc();
+        Runtime.getRuntime().gc();
+    }
+
+    private static double performanceCheckCached() {
+        final WritableImageCache cache = new WritableImageCache();
+        cache.add(new WritableImage(N_IMAGE_WIDTH, N_IMAGE_HEIGHT));
+
+        final long start = System.nanoTime();
+        for (int i = 0; i < N_ITERATIONS; i++) {
+            WritableImage image = cache.getImage(N_IMAGE_WIDTH, N_IMAGE_HEIGHT);
+
+            // simple check to minimise JIT optimisations
+            if ((int)image.getWidth() != N_IMAGE_WIDTH) {
+                throw new IllegalStateException("should not occur");
+            }
+
+            cache.add(image);
+            image = null;
+        }
+
+        final long stop = System.nanoTime();
+
+        return (stop - start) / (double) N_ITERATIONS;
+    }
+
+    private static double performanceCheckClassic() {
+        final long start = System.nanoTime();
+        for (int i = 0; i < N_ITERATIONS; i++) {
+            WritableImage image = new WritableImage(N_IMAGE_WIDTH, N_IMAGE_HEIGHT);
+
+            // simple check to minimise JIT optimisations
+            if ((int)image.getWidth() != N_IMAGE_WIDTH) {
+                throw new IllegalStateException("should not occur");
+            }
+
+            image = null;
+        }
+
+        final long stop = System.nanoTime();
+
+        return (stop - start) / (double) N_ITERATIONS;
+    }
+
+    public static void main(String[] args) {
+        final WritableImageCacheTests test = new WritableImageCacheTests();
+
+        test.testBasicFunction();
+        final double mem1 = Runtime.getRuntime().totalMemory();
+        final double classic = performanceCheckClassic();
+        final double mem2 = Runtime.getRuntime().totalMemory();
+        final double cached = performanceCheckCached();
+        final double mem3 = Runtime.getRuntime().totalMemory();
+        LOGGER.atInfo().addArgument(classic).addArgument(cached).log("WritableImageCache performance difference: {} ns (classic) vs. {} ns (cached)");
+        LOGGER.atInfo().addArgument(classic / cached * 100.0).log("WritableImageCache performance difference: {}& (classic/cached)");
+        LOGGER.atInfo().addArgument(mem1).log("memory before: {} Bytes");
+        LOGGER.atInfo().addArgument(mem2).log("after classic: {} Bytes");
+        LOGGER.atInfo().addArgument(mem3).log("after cached:  {} Bytes");
+    }
+}
---
+++ chartfx-dataset/src/main/java/de/gsi/dataset/utils/CacheCollection.java
@@ -0,0 +1,148 @@
+package de.gsi.dataset.utils;
+
+import java.lang.ref.Reference;
+import java.lang.ref.SoftReference;
+import java.util.AbstractCollection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Implements collection of cache-able objects that can be used to store recurring storage container.
+ * <p>
+ * N.B. this implements only the backing cache of adding, removing, etc. elements. The cache object retrieval should be implemented in the derived class.
+ * See for example {@link de.gsi.dataset.utils.ByteArrayCache}. 
+ * 
+ * @author rstein
+ *
+ * @param <T> generic for object type to be cahced.
+ */
+public class CacheCollection<T> extends AbstractCollection<T> {
+    protected final List<Reference<T>> contents = Collections.synchronizedList(new LinkedList<>());
+
+    @Override
+    public boolean add(T recoveredObject) {
+        if (recoveredObject != null) {
+            synchronized (contents) {
+                if (contains(recoveredObject)) {
+                    return false;
+                }
+                // N.B. here: specific choice of using 'SoftReferene'
+                // derived classes may overwrite this function and replace this with e.g. WeakReferene or simialr
+                return contents.add(new SoftReference<T>(recoveredObject));
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        synchronized (contents) {
+            contents.clear();
+        }
+    }
+
+    @Override
+    public boolean contains(Object object) {
+        if (object != null) {
+            synchronized (contents) {
+                for (Reference<T> weakReference : contents) {
+                    if (object.equals(weakReference.get()))
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        synchronized (contents) {
+            return new CacheCollectionIterator<>(contents.iterator());
+        }
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (o == null) {
+            return false;
+        }
+        synchronized (contents) {
+            Iterator<Reference<T>> iter = contents.iterator();
+            while (iter.hasNext()) {
+                final Reference<T> candidate = iter.next();
+                final T test = candidate.get();
+                if (test == null) {
+                    iter.remove();
+                    continue;
+                }
+                if (o.equals(test)) {
+                    iter.remove();
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        synchronized (contents) {
+            cleanup();
+            return contents.size();
+        }
+    }
+
+    protected void cleanup() {
+        synchronized (contents) {
+            List<Reference<T>> toRemove = new LinkedList<>();
+            for (Reference<T> weakReference : contents) {
+                if (weakReference.get() == null) {
+                    toRemove.add(weakReference);
+                }
+            }
+            contents.removeAll(toRemove);
+        }
+    }
+
+    private static class CacheCollectionIterator<T> implements Iterator<T> {
+        private final Iterator<Reference<T>> iterator;
+        private T next;
+
+        private CacheCollectionIterator(Iterator<Reference<T>> iterator) {
+            this.iterator = iterator;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (next != null) {
+                return true;
+            }
+            while (iterator.hasNext()) {
+                T t = iterator.next().get();
+                if (t != null) {
+                    // to ensure next() can't throw after hasNext() returned true, we need to dereference this
+                    next = t;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public T next() {
+            T result = next;
+            next = null;
+            while (result == null) {
+                result = iterator.next().get();
+            }
+            return result;
+        }
+
+        @Override
+        public void remove() {
+            iterator.remove();
+        }
+    }
+}
