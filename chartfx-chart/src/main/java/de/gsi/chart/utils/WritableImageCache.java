a61f41d0c660a39734d3f333606cb4c62b8f8906
rstein <r.steinhagen@gsi.de>
2020-04-13 19:20:08 +0200

    new CacheCollections, ByteArrayCache & WritableImageCache classes
    N.B. Implements cache collection to minimise memory re-allocation and in
    turn garbage collection for short-lived data storage objects. Other data
    storage contained can be similarly derived from CacheCollections. See
    ByteArrayCache for inspiration

---
+++ chartfx-chart/src/main/java/de/gsi/chart/utils/WritableImageCache.java
@@ -0,0 +1,70 @@
+package de.gsi.chart.utils;
+
+import java.lang.ref.Reference;
+
+import javafx.scene.image.WritableImage;
+
+import de.gsi.dataset.utils.CacheCollection;
+
+/**
+ * Implements WritableImage cache collection to minimise memory re-allocation.
+ *  
+ * <p> 
+ * N.B. This is useful to re-use short-lived data storage container to minimise the amount of garbage to be collected. This is used in situation replacing e.g.
+ * <pre>
+ *  {@code
+ *      public returnValue frequentlyExecutedFunction(...) {
+ *          final WritableImage storage = new WritableImage(width, heigth); // allocate new memory block (costly)
+ *          // [...] do short-lived computation on storage
+ *          // storage is implicitly finalised by garbage collector (costly)
+ *      }
+ *  }
+ * </pre>
+ * with
+ * <pre>
+ *  {@code
+ *      // ...
+ *      private final WritableImageCache cache = new WritableImageCache();
+ *      // ...
+ *      
+ *      public returnValue frequentlyExecutedFunction(...) {
+ *          final byte[] storage = cache.getImage(width, height); // return previously allocated image (cheap) or allocated new if necessary 
+ *          // [...] do short-lived computation on storage
+ *          cache.add(storage); // return object to cache
+ *      }
+ *  }
+ * </pre>
+ *  
+ * @author rstein
+ *
+ */
+public class WritableImageCache extends CacheCollection<WritableImage> {
+    public WritableImage getImage(final int requiredWidth, final int requiredHeight) {
+        synchronized (contents) {
+            WritableImage bestFit = null;
+
+            for (final Reference<WritableImage> candidate : contents) {
+                final WritableImage localRef = candidate.get();
+                if (localRef == null) {
+                    continue;
+                }
+                final int localWidth = (int) localRef.getWidth();
+                final int localHeight = (int) localRef.getHeight();
+
+                if (localWidth == requiredWidth && localHeight == requiredHeight) {
+                    bestFit = localRef;
+                    break;
+                }
+            }
+
+            if (bestFit == null) {
+                // could not find any cached, return new WritableImage
+                bestFit = new WritableImage(requiredWidth, requiredHeight);
+                return bestFit;
+            }
+
+            remove(bestFit);
+            return bestFit;
+        }
+    }
+}
