7f7b797b37037897c199d686a08f06f1e1f630a6
rstein <r.steinhagen@gsi.de>
2020-06-02 21:32:28 +0200

    allow AbstractSerialiser to serialise POJOs with null references
    N.B. null-references are ignored and not added to the byte buffer

--- chartfx-dataset/src/main/java/de/gsi/dataset/serializer/spi/AbstractSerialiser.java
+++ chartfx-dataset/src/main/java/de/gsi/dataset/serializer/spi/AbstractSerialiser.java
@@ -51,8 +51,8 @@ public abstract class AbstractSerialiser {
         }
 
         for (int i = 0; i < ref1.size(); i++) {
-            Class<?> class1 = ref1.get(i);
-            Class<?> class2 = ref2.get(i);
+            final Class<?> class1 = ref1.get(i);
+            final Class<?> class2 = ref2.get(i);
             if (!class1.equals(class2) && !(class2.isAssignableFrom(class1))) {
                 return false;
             }
@@ -62,7 +62,7 @@ public abstract class AbstractSerialiser {
     }
 
     public void checkSerialiserAvailability(final List<String> objList, final ClassFieldDescription field) {
-        boolean classIsKnown = isClassKnown(field.getType(), field.getActualTypeArguments());
+        final boolean classIsKnown = isClassKnown(field.getType(), field.getActualTypeArguments());
         if (field.isSerializable() && !classIsKnown) {
             if (field.getChildren().isEmpty()) {
                 final String fieldType = field.getTypeName() + field.getGenericFieldTypeString();
@@ -70,9 +70,7 @@ public abstract class AbstractSerialiser {
                     objList.add(fieldType);
                 }
             } else {
-                for (ClassFieldDescription child : field.getChildren()) {
-                    checkSerialiserAvailability(objList, child);
-                }
+                field.getChildren().forEach((final ClassFieldDescription child) -> checkSerialiserAvailability(objList, child));
             }
         }
     }
@@ -84,13 +82,13 @@ public abstract class AbstractSerialiser {
         if (clazz == null) {
             throw new IllegalArgumentException("clazz must not be null");
         }
-        List<FieldSerialiser> directClassMatchList = classMap.get(clazz);
+        final List<FieldSerialiser> directClassMatchList = classMap.get(clazz);
         if (directClassMatchList != null && !directClassMatchList.isEmpty()) {
             if (directClassMatchList.size() == 1 || classGenericArguments == null || classGenericArguments.isEmpty()) {
                 return Optional.of(directClassMatchList.get(0));
             }
             // more than one possible serialiser implementation
-            for (FieldSerialiser entry : directClassMatchList) {
+            for (final FieldSerialiser entry : directClassMatchList) {
                 if (checkClassCompatibility(classGenericArguments, entry.getGenericsPrototypes())) {
                     return Optional.of(entry);
                 }
@@ -98,28 +96,26 @@ public abstract class AbstractSerialiser {
             // found FieldSerialiser entry but not matching required generic types
         }
 
-        // did not find FieldSerialiser entry by specific class -> search for assignable
-        // interface definitions
+        // did not find FieldSerialiser entry by specific class -> search for assignable interface definitions
 
-        List<Class<?>> potentialMatchingKeys = this.knownClasses().keySet().stream().filter(k -> k.isAssignableFrom(clazz)).collect(Collectors.toList());
+        final List<Class<?>> potentialMatchingKeys = this.knownClasses().keySet().stream().filter(k -> k.isAssignableFrom(clazz)).collect(Collectors.toList());
         if (potentialMatchingKeys == null || potentialMatchingKeys.isEmpty()) {
             // did not find any matching clazz/interface FieldSerialiser entries
             return Optional.empty();
         }
 
-        List<FieldSerialiser> interfaceMatchList = potentialMatchingKeys.stream()
+        final List<FieldSerialiser> interfaceMatchList = potentialMatchingKeys.stream()
                                                            .map(key -> this.knownClasses().get(key))
                                                            .flatMap(List::stream)
                                                            .collect(Collectors.toList());
 
         if (interfaceMatchList.size() == 1 || classGenericArguments == null || classGenericArguments.isEmpty()) {
-            // found single match FieldSerialiser entry type w/o specific generics
-            // requirements
+            // found single match FieldSerialiser entry type w/o specific generics requirements
             return Optional.of(interfaceMatchList.get(0));
         }
 
         // more than one possible serialiser implementation
-        for (FieldSerialiser entry : interfaceMatchList) {
+        for (final FieldSerialiser entry : interfaceMatchList) {
             if (checkClassCompatibility(classGenericArguments, entry.getGenericsPrototypes())) {
                 // found generics matching or assignable entry
                 return Optional.of(entry);
@@ -127,15 +123,8 @@ public abstract class AbstractSerialiser {
         }
         // could not match with generics arguments
 
-        for (FieldSerialiser entry : interfaceMatchList) {
-            if (entry.getGenericsPrototypes().isEmpty()) {
-                // found generic serialiser entry w/o generics parameter requirements
-                return Optional.of(entry);
-            }
-        }
-
-        // did not find any FieldSerialiser entry
-        return Optional.empty();
+        // find generic serialiser entry w/o generics parameter requirements
+        return interfaceMatchList.stream().filter(entry -> entry.getGenericsPrototypes().isEmpty()).findFirst().map(Optional::of).orElse(Optional.empty());
     }
 
     public boolean isClassKnown(Class<?> clazz, List<Class<?>> classGenericArguments) {
@@ -150,31 +139,23 @@ public abstract class AbstractSerialiser {
         serialiseObject(obj, ClassDescriptions.get(obj.getClass()), 0);
     }
 
-    public void serialiseObject(final Object obj, final ClassFieldDescription root, final int recursionDepth)
+    public void serialiseObject(final Object rootObj, final ClassFieldDescription root, final int recursionDepth)
             throws IllegalAccessException {
         final Class<?> fieldClass = root.getType();
-        List<Class<?>> fieldGenericTypes = root.getActualTypeArguments();
+        final List<Class<?>> fieldGenericTypes = root.getActualTypeArguments();
 
         if (isClassKnown(fieldClass, fieldGenericTypes) && recursionDepth != 0) {
             // serialise known class object
-            Optional<FieldSerialiser> serialiser = findFieldSerialiserForKnownClassOrInterface(fieldClass,
-                    root.getActualTypeArguments());
-
-            if (serialiser.isPresent()) {
-                Object classObj = root.getMemberClassObject(obj, false);
-                if (classObj == null) {
-                    // do not serialise 'null' references
-                    return;
-                }
-                serialiser.get().getWriterFunction().exec(classObj, root);
-                return;
+            final Optional<FieldSerialiser> serialiser = findFieldSerialiserForKnownClassOrInterface(fieldClass, root.getActualTypeArguments());
+            if (serialiser.isEmpty()) {
+                // should not happen (because of 'isClassKnown')
+                throw new IllegalStateException("should not happen -- cannot serialise field - " + root.getFieldNameRelative() + " - class type = " + root.getTypeName());
             }
 
-            // should not happen (because of 'isClassKnown')
-            throw new IllegalStateException("should not happen -- cannot serialise field - " + root.getFieldNameRelative() + " - class type = " + root.getTypeName());
+            serialiser.get().getWriterFunction().exec(rootObj, root);
+            return;
         }
-        // cannot serialise field check whether this is a container class and contains
-        // serialisable children
+        // cannot serialise field check whether this is a container class and contains serialisable children
 
         if (root.getChildren().isEmpty()) {
             // no further children
@@ -187,8 +168,14 @@ public abstract class AbstractSerialiser {
             startMarkerFunction.accept(subClass);
         }
 
+        final Object newRoot = root.getField() == null ? rootObj : root.getField().get(rootObj);
         for (final ClassFieldDescription field : root.getChildren()) {
-            serialiseObject(obj, field, recursionDepth + 1);
+            final Object reference = field.getField().get(newRoot);
+            if (reference == null) {
+                // only follow and serialise non-null references of sub-classes
+                continue;
+            }
+            serialiseObject(newRoot, field, recursionDepth + 1);
         }
 
         if (recursionDepth != 0 && endMarkerFunction != null) {
@@ -203,7 +190,7 @@ public abstract class AbstractSerialiser {
         if (classGenericArguments == null || classGenericArguments.isEmpty()) {
             return result;
         }
-        for (Type arg : classGenericArguments) {
+        for (final Type arg : classGenericArguments) {
             result = prime * result + ((arg == null) ? 0 : arg.getTypeName().hashCode());
         }
 
@@ -217,9 +204,7 @@ public abstract class AbstractSerialiser {
                 return (Constructor<Object>) ClassDescriptions.getClassByName(key)
                         .getDeclaredConstructor(parameterTypes);
             } catch (SecurityException | NoSuchMethodException e) {
-                if (LOGGER.isErrorEnabled()) {
-                    LOGGER.atError().setCause(e).addArgument(Arrays.toString(parameterTypes)).addArgument(name).log("exception while getting constructor{} for class {}");
-                }
+                LOGGER.atError().setCause(e).addArgument(Arrays.toString(parameterTypes)).addArgument(name).log("exception while getting constructor{} for class {}");
                 return null;
             }
         });
@@ -229,7 +214,7 @@ public abstract class AbstractSerialiser {
         if (classGenericArguments == null) {
             return new String[0];
         }
-        String[] argStrings = new String[classGenericArguments.size()];
+        final String[] argStrings = new String[classGenericArguments.size()];
         for (int i = 0; i < argStrings.length; i++) {
             argStrings[i] = classGenericArguments.get(i).getName();
         }
