6adeea33de6724107870da8e3efaf61341d14ca0
rstein <r.steinhagen@gsi.de>
2020-06-22 11:54:03 +0200

    update LaunchJFX to include notes for Intellij and some add. safe-guards

--- chartfx-samples/src/main/java/de/gsi/samples/util/LaunchJFX.java
+++ chartfx-samples/src/main/java/de/gsi/samples/util/LaunchJFX.java
@@ -2,15 +2,24 @@ package de.gsi.samples.util;
 
 import javafx.application.Application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.gsi.chart.samples.ErrorDataSetRendererSample;
 import de.gsi.chart.samples.RunChartSamples;
 
 /**
- * Helper class to launch JavaFX applications without manually adding modules and exports to the java command line. Add
- * a run configuration for this class in eclipse and in the arguments tab, add "${java_type_name}" as program argument.
+ * Helper class to launch JavaFX applications without manually adding modules and exports to the java command line.
+ * Add a run configuration for this class in the arguments tab and add as program argument:
+ * <ul>
+ *     <li>for Eclipse IDE: "${java_type_name}"</li>
+ *     <li>for Intellij IDE: "$FileClass$"</li>
+ * </ul>
+ *
  * Then, any JavaFX Application Class can be run by selecting it in the Package Explorer and running this run
  * configuration. <br>
  * To be able to run JavaFX Applications outside of the chartfx project also add a classpath entry of type "Advanced",
- * "Variable", "${project_classpat}", but there is no way to add the current projects dependencies to the classpath,
+ * "Variable", "${project_classpath}", but there is no way to add the current projects dependencies to the classpath,
  * yet.
  * 
  * @see <a href="https://stackoverflow.com/a/55300492" target="_top"> Stackoverflow: How to add JavaFX runtime to
@@ -18,11 +27,18 @@ import de.gsi.chart.samples.RunChartSamples;
  * @author akrimm
  */
 public class LaunchJFX { // NOMEN EST OMEN
+    private static final Logger LOGGER = LoggerFactory.getLogger(LaunchJFX.class);
     public static void main(final String[] args) throws ClassNotFoundException {
-        if (args.length < 1) {
+        if (args.length < 1 || args[0].contains(LaunchJFX.class.getName())) {
             Application.launch(RunChartSamples.class);
         } else {
-            Application.launch(Class.forName(args[0]).asSubclass(Application.class));
+            Class<? extends Application> clazz = Class.forName(args[0]).asSubclass(Application.class);
+            if (Application.class.isAssignableFrom(clazz)) {
+                Application.launch(clazz);
+            } else {
+                LOGGER.atInfo().addArgument(clazz).log("{} is not an Application - starting default view");
+                Application.launch(RunChartSamples.class);
+            }
         }
     }
 }
