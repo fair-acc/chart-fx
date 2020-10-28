11fa07312322e644bc3480c667555313757e1e22
Alexander Krimm <A.Krimm@gsi.de>
2020-10-28 11:32:11 +0100

    Add simple sample for demuxing events potentially arriving out of order and interleaved

---
+++ microservice/src/test/java/de/gsi/microservice/concepts/aggregate/demux/poc/TestEventSourceSample.java
@@ -0,0 +1,29 @@
+package de.gsi.microservice.concepts.aggregate.demux.poc;
+
+import com.lmax.disruptor.RingBuffer;
+import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.util.DaemonThreadFactory;
+
+public class TestEventSourceSample {
+    private final TestEventSource testEventSource;
+
+    public TestEventSourceSample() {
+        final Disruptor<TestEventSource.IngestedEvent> disruptor = new Disruptor<>(TestEventSource.IngestedEvent::new, 256, DaemonThreadFactory.INSTANCE);
+        final DemuxProcessor aggProc = new DemuxProcessor(disruptor.getRingBuffer());
+        disruptor.handleEventsWith(((event, sequence, endOfBatch) -> System.out.println(sequence + "@" + event.ingestionTime + ": " + event.payload)))
+                .handleEventsWith(aggProc)
+                .then(aggProc.workers)
+                .asSequenceBarrier();
+
+        final RingBuffer<TestEventSource.IngestedEvent> rb = disruptor.start();
+        testEventSource = new TestEventSource(TestEventSource.overlapping, true, rb);
+    }
+
+    public void run() {
+        testEventSource.run();
+    }
+
+    public static void main(String[] args) {
+        new TestEventSourceSample().run();
+    }
+}
