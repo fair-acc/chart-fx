fc81f1fa2fced4d4102bbc4fa3558cef35742a7e
Alexander Krimm <A.Krimm@gsi.de>
2020-12-18 09:41:42 +0100

    Disable verbose logging in tests
    Disabling trace output in unittests to keep the build logs more
    manageable.

--- chartfx-chart/src/test/java/de/gsi/chart/renderer/spi/financial/CandleStickRendererTest.java
+++ chartfx-chart/src/test/java/de/gsi/chart/renderer/spi/financial/CandleStickRendererTest.java
@@ -42,7 +42,7 @@ public class CandleStickRendererTest {
 
     @Start
     public void start(Stage stage) throws Exception {
-        ProcessingProfiler.setDebugState(true);
+        ProcessingProfiler.setDebugState(false); // enable for detailed renderer tracing
         ohlcvDataSet = new OhlcvDataSet("ohlc1");
         ohlcvDataSet.setData(FinancialTestUtils.createTestOhlcv());
         rendererTested = new CandleStickRenderer(true);
