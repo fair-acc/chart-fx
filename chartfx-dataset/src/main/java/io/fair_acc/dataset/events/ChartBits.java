23ce3a6c3057e7179659519b0f6e18c05a990a9e
Florian Enner <florian@hebirobotics.com>
2023-07-22 08:04:20 +0200

    added debug printer

--- chartfx-dataset/src/main/java/io/fair_acc/dataset/events/ChartBits.java
+++ chartfx-dataset/src/main/java/io/fair_acc/dataset/events/ChartBits.java
@@ -22,8 +22,37 @@ public enum ChartBits implements IntSupplier {
         return (bit & mask) != 0;
     }
 
+    private int clear(int mask) {
+        return mask & ~bit;
+    }
+
+    public static StateListener printer() {
+        return (source, bits) -> System.out.println(toString(source, bits));
+    }
+
+    public static String toString(BitState bitState, int bits) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(bitState.getSource()).append(" ");
+        if(bits == 0) {
+            return builder.append("clean").toString();
+        }
+        builder.append("dirty[");
+        for (ChartBits bit : knownBits) {
+            if(bit.isSet(bits)) {
+                builder.append(bit.name()).append(", ");
+                bits = bit.clear(bits);
+            }
+        }
+        if (bits != 0) {
+            builder.append("UNKNOWN, ");
+        }
+        builder.setLength(builder.length() - 2);
+        return builder.append("]").toString();
+    }
+
     final int bit = 1 << ordinal();
 
-    public static final int ANY = BitState.mask(ChartBits.values());
+    private static final ChartBits[] knownBits = ChartBits.values();
+    public static final int ANY = BitState.mask(knownBits);
 
 }
