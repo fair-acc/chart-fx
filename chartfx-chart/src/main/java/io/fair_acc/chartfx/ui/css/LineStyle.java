1e137d39b4dde40b278457f0abb4785171b5c9c4
Florian Enner <florian@hebirobotics.com>
2023-08-05 01:39:24 +0200

    cleaned up axis styling
    (cherry picked from commit cede075bccbfc78321252f691f7d1cd08fd63920)

--- chartfx-chart/src/main/java/io/fair_acc/chartfx/ui/css/LineStyle.java
+++ chartfx-chart/src/main/java/io/fair_acc/chartfx/ui/css/LineStyle.java
@@ -3,27 +3,21 @@ package io.fair_acc.chartfx.ui.css;
 import javafx.beans.property.LongProperty;
 import javafx.beans.property.ReadOnlyLongProperty;
 import javafx.beans.property.SimpleLongProperty;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.shape.Line;
 
 /**
- * An invisible node that lets users change styles
- * via CSS. The actual drawing is done in a canvas to
- * reduce the number of nodes on the SceneGraph. Each change
- * increments a counter, so that it is easy to invalidate outdated
- * renderings.
- *
  * @author ennerf
  */
-public class LineStyle extends Line implements StyleUtil.ChangeCounter {
+public class LineStyle extends Line implements StyleUtil.StyleNode {
 
     public LineStyle(String... styles) {
         StyleUtil.styleNode(this, styles);
-        StyleUtil.registerShapeListener(this, StyleUtil.incrementOnChange(changeCounter));
+        StyleUtil.forEachStyleProp(this, StyleUtil.incrementOnChange(changeCounter));
     }
 
-    public void copyStyleTo(GraphicsContext gc) {
-        StyleUtil.copyShapeStyle(this, gc);
+    @Override
+    public String toString() {
+        return StyleUtil.toStyleString(this);
     }
 
     public ReadOnlyLongProperty changeCounterProperty() {
