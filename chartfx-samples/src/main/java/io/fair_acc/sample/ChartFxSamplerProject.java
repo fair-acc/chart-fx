5ff5785d057a03b52611b92782510e10fd666795
Florian Enner <florian@hebirobotics.com>
2023-08-16 10:09:37 +0200

    added buttons for scenic view and CSSFX to sampler welcome page

--- chartfx-samples/src/main/java/io/fair_acc/sample/ChartFxSamplerProject.java
+++ chartfx-samples/src/main/java/io/fair_acc/sample/ChartFxSamplerProject.java
@@ -1,11 +1,17 @@
 package io.fair_acc.sample;
 
+import fr.brouillard.oss.cssfx.CSSFX;
 import fxsampler.FXSamplerProject;
+import io.fair_acc.chartfx.Chart;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import fxsampler.model.WelcomePage;
+import org.scenicview.ScenicView;
+
+import java.util.Objects;
 
 public class ChartFxSamplerProject implements FXSamplerProject {
 
@@ -39,7 +45,30 @@ public class ChartFxSamplerProject implements FXSamplerProject {
         label.setWrapText(true);
         label.setText("Welcome to ChartFx samples!\nThis library provides a wide array of facilities for high performance scientific plotting.\n\n Explore the available chart controls by clicking on the options to the left.");
         label.setStyle("-fx-font-size: 1.5em; -fx-padding: 20 0 0 5;");
-        vBox.getChildren().addAll(pane, label);
+        vBox.setStyle("-fx-padding: 5px; -fx-spacing: 5px");
+
+        var scenicView = new Button("Show ScenicView");
+        scenicView.setOnAction(a -> ScenicView.show(scenicView.getScene()));
+
+        var addDefaultCss = new Button("Add chart.css");
+        addDefaultCss.setOnAction(a -> {
+            // agent stylesheets don't get reloaded, so we need to manually add the css file
+            // to make CSSFX work with "mvn -pl chartfx-chart sass-cli:watch"
+            addDefaultCss.getScene().getStylesheets().add(Objects.requireNonNull(Chart.class.getResource("chart.css")).toExternalForm());
+            addDefaultCss.setDisable(true);
+        });
+        addDefaultCss.managedProperty().bind(addDefaultCss.visibleProperty());
+        addDefaultCss.setVisible(false);
+
+        var cssFx = new Button("Start CSSFX");
+        cssFx.setOnAction(a -> {
+            cssFx.getScene().getStylesheets().add(Objects.requireNonNull(Chart.class.getResource("chart.css")).toExternalForm());
+            CSSFX.start(cssFx.getScene());
+            cssFx.setDisable(true);
+            addDefaultCss.setVisible(true);
+        });
+
+        vBox.getChildren().addAll(pane, label, scenicView, cssFx, addDefaultCss);
         return new WelcomePage("Welcome to ChartFx!", vBox);
     }
 }
