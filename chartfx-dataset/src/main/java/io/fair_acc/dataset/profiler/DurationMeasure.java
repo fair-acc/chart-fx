a76c573fa6518fd1fb7101a59a7408f7833bb8cd
Florian Enner <florian@hebirobotics.com>
2023-08-20 16:31:29 +0200

    added better support for recording durations coming from external timestamps

--- chartfx-dataset/src/main/java/io/fair_acc/dataset/profiler/DurationMeasure.java
+++ chartfx-dataset/src/main/java/io/fair_acc/dataset/profiler/DurationMeasure.java
@@ -1,11 +1,13 @@
 package io.fair_acc.dataset.profiler;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Gets called before and after an action. May record time.
  *
  * @author ennerf
  */
-public interface DurationMeasure {
+public interface DurationMeasure extends LongMeasure {
 
     /**
      * Called when an action begins. Sets the start timestamp.
@@ -16,6 +18,11 @@ public interface DurationMeasure {
      * Called when an action is done. Records delta from the start timestamp.
      */
     void stop();
+
+    /**
+     * @return timeUnit of the used clock
+     */
+    public TimeUnit getClockUnit();
 
     /**
      * Calling stop without start is typically an invalid call that may throw an
@@ -32,13 +39,23 @@ public interface DurationMeasure {
      */
     public static final DurationMeasure DISABLED = new DurationMeasure() {
         @Override
+        public void recordRawValue(long value) {
+            // no-op
+        }
+
+        @Override
         public void start() {
-            //no-op
+            // no-op
         }
 
         @Override
         public void stop() {
             // no-op
+        }
+
+        @Override
+        public TimeUnit getClockUnit() {
+            return TimeUnit.NANOSECONDS;
         }
     };
 
