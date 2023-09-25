02aa9de9ca141759c6f08f18d5c4ca0d056e4b52
Florian Enner <florian@hebirobotics.com>
2023-09-26 00:07:42 +0200

    added optional debug prints
    (cherry picked from commit 74ec30a828b12af6cce1bb19c265e8085375d510)

--- chartfx-samples/src/main/java/io/fair_acc/sample/ChartFxSampler.java
+++ chartfx-samples/src/main/java/io/fair_acc/sample/ChartFxSampler.java
@@ -2,8 +2,33 @@ package io.fair_acc.sample;
 
 import fxsampler.FXSampler;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Optional;
+
 public class ChartFxSampler {
+
+    private static boolean debug = Optional.ofNullable(System.getenv("chartfx.debug"))
+            .map(Boolean::parseBoolean)
+            .orElse(false);
+
     public static void main(String[] args) {
+        if (debug) {
+            // Debug output for checking classpath
+            System.out.println("\nJars on ClassPath:");
+            String classpath = System.getProperty("java.class.path");
+            Arrays.stream(classpath.split(File.pathSeparator))
+                    .map(String::toString)
+                    .filter(str -> str.endsWith(".jar"))
+                    .map(str -> "* " + str.substring(str.lastIndexOf(File.separatorChar) + 1))
+                    .sorted()
+                    .forEach(System.out::println);
+
+            // The FX Sampler only searches for jars/classes at and below the working directory
+            System.out.println("\nworkingDir = " + Path.of(".").toAbsolutePath().normalize() + "\n");
+        }
+
         FXSampler.main(args);
     }
 }
